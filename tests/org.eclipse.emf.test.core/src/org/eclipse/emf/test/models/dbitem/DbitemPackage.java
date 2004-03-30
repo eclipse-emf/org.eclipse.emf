c455f2530019f6ff49cc2a16cb13d82be49b56f5
elena <elena>
2004-03-30 00:12:06 +0000

    Add dbitem model for testing namespace scope

---
+++ tests/org.eclipse.emf.test.core/src/org/eclipse/emf/test/models/dbitem/DbitemPackage.java
@@ -0,0 +1,290 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: DbitemPackage.java,v 1.1 2004/03/30 00:12:06 elena Exp $
+ */
+package org.eclipse.emf.test.models.dbitem;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Package</b> for the model.
+ * It contains accessors for the meta objects to represent
+ * <ul>
+ *   <li>each class,</li>
+ *   <li>each feature of each class,</li>
+ *   <li>each enum,</li>
+ *   <li>and each data type</li>
+ * </ul>
+ * <!-- end-user-doc -->
+ * @see org.eclipse.emf.test.models.dbitem.DbitemFactory
+ * @generated
+ */
+public interface DbitemPackage extends EPackage
+{
+  /**
+   * The package name.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  String eNAME = "dbitem";
+
+  /**
+   * The package namespace URI.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  String eNS_URI = "http://org/eclipse/emf/test/models/dbitem";
+
+  /**
+   * The package namespace name.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  String eNS_PREFIX = "dbitem";
+
+  /**
+   * The singleton instance of the package.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  DbitemPackage eINSTANCE = org.eclipse.emf.test.models.dbitem.impl.DbitemPackageImpl.init();
+
+  /**
+   * The meta object id for the '{@link org.eclipse.emf.test.models.dbitem.impl.DbTypeImpl <em>Db Type</em>}' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see org.eclipse.emf.test.models.dbitem.impl.DbTypeImpl
+   * @see org.eclipse.emf.test.models.dbitem.impl.DbitemPackageImpl#getDbType()
+   * @generated
+   */
+  int DB_TYPE = 0;
+
+  /**
+   * The feature id for the '<em><b>Red Pen</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DB_TYPE__RED_PEN = 0;
+
+  /**
+   * The feature id for the '<em><b>Blue Pen</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DB_TYPE__BLUE_PEN = 1;
+
+  /**
+   * The feature id for the '<em><b>Red Pencil</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DB_TYPE__RED_PENCIL = 2;
+
+  /**
+   * The feature id for the '<em><b>Blue Pencil</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DB_TYPE__BLUE_PENCIL = 3;
+
+  /**
+   * The number of structural features of the the '<em>Db Type</em>' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DB_TYPE_FEATURE_COUNT = 4;
+
+  /**
+   * The meta object id for the '{@link org.eclipse.emf.test.models.dbitem.impl.DocumentRootImpl <em>Document Root</em>}' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see org.eclipse.emf.test.models.dbitem.impl.DocumentRootImpl
+   * @see org.eclipse.emf.test.models.dbitem.impl.DbitemPackageImpl#getDocumentRoot()
+   * @generated
+   */
+  int DOCUMENT_ROOT = 1;
+
+  /**
+   * The feature id for the '<em><b>Mixed</b></em>' attribute list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DOCUMENT_ROOT__MIXED = 0;
+
+  /**
+   * The feature id for the '<em><b>XMLNS Prefix Map</b></em>' map.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DOCUMENT_ROOT__XMLNS_PREFIX_MAP = 1;
+
+  /**
+   * The feature id for the '<em><b>XSI Schema Location</b></em>' map.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DOCUMENT_ROOT__XSI_SCHEMA_LOCATION = 2;
+
+  /**
+   * The feature id for the '<em><b>Dbitem</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DOCUMENT_ROOT__DBITEM = 3;
+
+  /**
+   * The number of structural features of the the '<em>Document Root</em>' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int DOCUMENT_ROOT_FEATURE_COUNT = 4;
+
+
+  /**
+   * Returns the meta object for class '{@link org.eclipse.emf.test.models.dbitem.DbType <em>Db Type</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for class '<em>Db Type</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DbType
+   * @generated
+   */
+  EClass getDbType();
+
+  /**
+   * Returns the meta object for the containment reference '{@link org.eclipse.emf.test.models.dbitem.DbType#getRedPen <em>Red Pen</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the containment reference '<em>Red Pen</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DbType#getRedPen()
+   * @see #getDbType()
+   * @generated
+   */
+  EReference getDbType_RedPen();
+
+  /**
+   * Returns the meta object for the containment reference '{@link org.eclipse.emf.test.models.dbitem.DbType#getBluePen <em>Blue Pen</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the containment reference '<em>Blue Pen</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DbType#getBluePen()
+   * @see #getDbType()
+   * @generated
+   */
+  EReference getDbType_BluePen();
+
+  /**
+   * Returns the meta object for the containment reference '{@link org.eclipse.emf.test.models.dbitem.DbType#getRedPencil <em>Red Pencil</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the containment reference '<em>Red Pencil</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DbType#getRedPencil()
+   * @see #getDbType()
+   * @generated
+   */
+  EReference getDbType_RedPencil();
+
+  /**
+   * Returns the meta object for the containment reference '{@link org.eclipse.emf.test.models.dbitem.DbType#getBluePencil <em>Blue Pencil</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the containment reference '<em>Blue Pencil</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DbType#getBluePencil()
+   * @see #getDbType()
+   * @generated
+   */
+  EReference getDbType_BluePencil();
+
+  /**
+   * Returns the meta object for class '{@link org.eclipse.emf.test.models.dbitem.DocumentRoot <em>Document Root</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for class '<em>Document Root</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DocumentRoot
+   * @generated
+   */
+  EClass getDocumentRoot();
+
+  /**
+   * Returns the meta object for the attribute list '{@link org.eclipse.emf.test.models.dbitem.DocumentRoot#getMixed <em>Mixed</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the attribute list '<em>Mixed</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DocumentRoot#getMixed()
+   * @see #getDocumentRoot()
+   * @generated
+   */
+  EAttribute getDocumentRoot_Mixed();
+
+  /**
+   * Returns the meta object for the map '{@link org.eclipse.emf.test.models.dbitem.DocumentRoot#getXMLNSPrefixMap <em>XMLNS Prefix Map</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the map '<em>XMLNS Prefix Map</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DocumentRoot#getXMLNSPrefixMap()
+   * @see #getDocumentRoot()
+   * @generated
+   */
+  EReference getDocumentRoot_XMLNSPrefixMap();
+
+  /**
+   * Returns the meta object for the map '{@link org.eclipse.emf.test.models.dbitem.DocumentRoot#getXSISchemaLocation <em>XSI Schema Location</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the map '<em>XSI Schema Location</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DocumentRoot#getXSISchemaLocation()
+   * @see #getDocumentRoot()
+   * @generated
+   */
+  EReference getDocumentRoot_XSISchemaLocation();
+
+  /**
+   * Returns the meta object for the containment reference '{@link org.eclipse.emf.test.models.dbitem.DocumentRoot#getDbitem <em>Dbitem</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the containment reference '<em>Dbitem</em>'.
+   * @see org.eclipse.emf.test.models.dbitem.DocumentRoot#getDbitem()
+   * @see #getDocumentRoot()
+   * @generated
+   */
+  EReference getDocumentRoot_Dbitem();
+
+  /**
+   * Returns the factory that creates the instances of the model.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the factory that creates the instances of the model.
+   * @generated
+   */
+  DbitemFactory getDbitemFactory();
+
+} //DbitemPackage
---
+++ tests/org.eclipse.emf.test.core/src/org/eclipse/emf/test/models/dbprice/impl/DbpricePackageImpl.java
@@ -0,0 +1,289 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: DbpricePackageImpl.java,v 1.1 2004/03/30 00:12:06 elena Exp $
+ */
+package org.eclipse.emf.test.models.dbprice.impl;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EPackageImpl;
+
+import org.eclipse.emf.ecore.xml.type.XMLTypePackage;
+
+import org.eclipse.emf.ecore.xml.type.impl.XMLTypePackageImpl;
+
+import org.eclipse.emf.test.models.dbitem.DbitemPackage;
+
+import org.eclipse.emf.test.models.dbitem.impl.DbitemPackageImpl;
+
+import org.eclipse.emf.test.models.dbprice.DbpriceFactory;
+import org.eclipse.emf.test.models.dbprice.DbpricePackage;
+import org.eclipse.emf.test.models.dbprice.PenType;
+import org.eclipse.emf.test.models.dbprice.PencilType;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Package</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class DbpricePackageImpl extends EPackageImpl implements DbpricePackage
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  private EClass pencilTypeEClass = null;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  private EClass penTypeEClass = null;
+
+  /**
+   * Creates an instance of the model <b>Package</b>, registered with
+   * {@link org.eclipse.emf.ecore.EPackage.Registry EPackage.Registry} by the package
+   * package URI value.
+   * <p>Note: the correct way to create the package is via the static
+   * factory method {@link #init init()}, which also performs
+   * initialization of the package, or returns the registered package,
+   * if one already exists.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see org.eclipse.emf.ecore.EPackage.Registry
+   * @see org.eclipse.emf.test.models.dbprice.DbpricePackage#eNS_URI
+   * @see #init()
+   * @generated
+   */
+  private DbpricePackageImpl()
+  {
+    super(eNS_URI, DbpriceFactory.eINSTANCE);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  private static boolean isInited = false;
+
+  /**
+   * Creates, registers, and initializes the <b>Package</b> for this
+   * model, and for any others upon which it depends.  Simple
+   * dependencies are satisfied by calling this method on all
+   * dependent packages before doing anything else.  This method drives
+   * initialization for interdependent packages directly, in parallel
+   * with this package, itself.
+   * <p>Of this package and its interdependencies, all packages which
+   * have not yet been registered by their URI values are first created
+   * and registered.  The packages are then initialized in two steps:
+   * meta-model objects for all of the packages are created before any
+   * are initialized, since one package's meta-model objects may refer to
+   * those of another.
+   * <p>Invocation of this method will not affect any packages that have
+   * already been initialized.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #eNS_URI
+   * @see #createPackageContents()
+   * @see #initializePackageContents()
+   * @generated
+   */
+  public static DbpricePackage init()
+  {
+    if (isInited) return (DbpricePackage)EPackage.Registry.INSTANCE.get(DbpricePackage.eNS_URI);
+
+    // Obtain or create and register package.
+    DbpricePackageImpl theDbpricePackage = (DbpricePackageImpl)(EPackage.Registry.INSTANCE.get(eNS_URI) instanceof EPackage ? EPackage.Registry.INSTANCE.get(eNS_URI) : new DbpricePackageImpl());
+
+    isInited = true;
+
+    // Initialize simple dependencies
+    XMLTypePackageImpl.init();
+
+    // Obtain or create and register interdependencies
+    DbitemPackageImpl theDbitemPackage = (DbitemPackageImpl)(EPackage.Registry.INSTANCE.get(DbitemPackage.eNS_URI) instanceof EPackage ? EPackage.Registry.INSTANCE.get(DbitemPackage.eNS_URI) : DbitemPackageImpl.eINSTANCE);
+
+    // Step 1: create meta-model objects
+    theDbpricePackage.createPackageContents();
+    theDbitemPackage.createPackageContents();
+
+    // Step 2: complete initialization
+    theDbpricePackage.initializePackageContents();
+    theDbitemPackage.initializePackageContents();
+
+    return theDbpricePackage;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EClass getPencilType()
+  {
+    return pencilTypeEClass;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EAttribute getPencilType_Price()
+  {
+    return (EAttribute)pencilTypeEClass.getEStructuralFeatures().get(0);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EClass getPenType()
+  {
+    return penTypeEClass;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EAttribute getPenType_Price()
+  {
+    return (EAttribute)penTypeEClass.getEStructuralFeatures().get(0);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public DbpriceFactory getDbpriceFactory()
+  {
+    return (DbpriceFactory)getEFactoryInstance();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  private boolean isCreated = false;
+
+  /**
+   * Creates the meta-model objects for the package.  This method is
+   * guarded to have no affect on any invocation but its first.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void createPackageContents()
+  {
+    if (isCreated) return;
+    isCreated = true;
+
+    // Create classes and their features
+    pencilTypeEClass = createEClass(PENCIL_TYPE);
+    createEAttribute(pencilTypeEClass, PENCIL_TYPE__PRICE);
+
+    penTypeEClass = createEClass(PEN_TYPE);
+    createEAttribute(penTypeEClass, PEN_TYPE__PRICE);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  private boolean isInitialized = false;
+
+  /**
+   * Complete the initialization of the package and its meta-model.  This
+   * method is guarded to have no affect on any invocation but its first.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void initializePackageContents()
+  {
+    if (isInitialized) return;
+    isInitialized = true;
+
+    // Initialize package
+    setName(eNAME);
+    setNsPrefix(eNS_PREFIX);
+    setNsURI(eNS_URI);
+
+    // Obtain other dependent packages
+    XMLTypePackageImpl theXMLTypePackage = (XMLTypePackageImpl)EPackage.Registry.INSTANCE.getEPackage(XMLTypePackage.eNS_URI);
+
+    // Add supertypes to classes
+
+    // Initialize classes and features; add operations and parameters
+    initEClass(pencilTypeEClass, PencilType.class, "PencilType", !IS_ABSTRACT, !IS_INTERFACE);
+    initEAttribute(getPencilType_Price(), theXMLTypePackage.getInt(), "price", null, 1, 1, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, IS_UNSETTABLE, !IS_ID, !IS_UNIQUE, !IS_DERIVED);
+
+    initEClass(penTypeEClass, PenType.class, "PenType", !IS_ABSTRACT, !IS_INTERFACE);
+    initEAttribute(getPenType_Price(), theXMLTypePackage.getInt(), "price", null, 1, 1, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, IS_UNSETTABLE, !IS_ID, !IS_UNIQUE, !IS_DERIVED);
+
+    // Create resource
+    createResource(eNS_URI);
+
+    // Create annotations
+    // http:///org/eclipse/emf/ecore/util/ExtendedMetaData
+    createExtendedMetaDataAnnotations();
+  }
+
+  /**
+   * Initializes the annotations for <b>http:///org/eclipse/emf/ecore/util/ExtendedMetaData</b>.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected void createExtendedMetaDataAnnotations()
+  {
+    String source = "http:///org/eclipse/emf/ecore/util/ExtendedMetaData";		
+    addAnnotation
+      (pencilTypeEClass, 
+       source, 
+       new String[] 
+       {
+       "name", "pencilType",
+       "kind", "elementOnly"
+       });		
+    addAnnotation
+      (getPencilType_Price(), 
+       source, 
+       new String[] 
+       {
+       "kind", "element",
+       "name", "price"
+       });		
+    addAnnotation
+      (penTypeEClass, 
+       source, 
+       new String[] 
+       {
+       "name", "penType",
+       "kind", "elementOnly"
+       });		
+    addAnnotation
+      (getPenType_Price(), 
+       source, 
+       new String[] 
+       {
+       "kind", "element",
+       "name", "price",
+       "namespace", "##targetNamespace"
+       });
+  }
+} //DbpricePackageImpl
