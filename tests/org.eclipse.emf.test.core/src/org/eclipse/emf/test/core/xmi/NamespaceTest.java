031813e03d73409c22fbde6537eb051fb40de52c
elena <elena>
2004-03-30 00:12:39 +0000

    Add tests for namespaces in scope

---
+++ tests/org.eclipse.emf.test.core/src/org/eclipse/emf/test/core/xmi/NamespaceTest.java
@@ -0,0 +1,101 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2002-2004 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: NamespaceTest.java,v 1.1 2004/03/30 00:12:39 elena Exp $
+ */
+
+package org.eclipse.emf.test.core.xmi;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Collections;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.test.core.EMFTestCorePlugin;
+import org.eclipse.emf.test.models.dbitem.DbitemPackage;
+import org.eclipse.emf.test.models.dbitem.util.DbitemResourceFactoryImpl;
+import org.eclipse.emf.test.models.dbprice.DbpricePackage;
+
+
+public class NamespaceTest extends TestCase
+{
+  String file = null;
+  ResourceSet rs;
+
+  public NamespaceTest(String name)
+  {
+    super(name);
+  }
+
+  public static Test suite()
+  {
+    TestSuite ts = new TestSuite();
+    ts.addTest(new NamespaceTest("fileisLoadXML"));
+    ts.addTest(new NamespaceTest("byteisLoadXML"));
+    return ts;
+  }
+
+  /**
+   * @see junit.framework.TestCase#setUp()
+   */
+  protected void setUp() throws Exception
+  {
+    rs = new ResourceSetImpl();
+    DbpricePackage p1 = DbpricePackage.eINSTANCE;
+    DbitemPackage p2 = DbitemPackage.eINSTANCE;
+    rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", new DbitemResourceFactoryImpl());
+    file = EMFTestCorePlugin.getPlugin().getPluginDirectory() + "/data/xmi/dbitemtest.xml";
+  }
+
+  /**
+   * @see junit.framework.TestCase#tearDown()
+   */
+  protected void tearDown() throws Exception
+  {
+    rs = null;
+  }
+
+  public void fileisLoadXML() throws MalformedURLException, IOException
+  {
+    FileInputStream fi = null;
+    Resource r = rs.createResource(URI.createURI(".xml"));
+
+    fi = new FileInputStream(file);
+    r.load(fi, Collections.EMPTY_MAP);
+  }
+
+  public void byteisLoadXML() throws MalformedURLException, IOException
+  {
+    byte[] bbuffer = new byte [2064];
+    int length = 0;
+
+    java.net.URL u = new java.net.URL("file:///" + file);
+    java.io.InputStream i = u.openStream();
+    length = i.read(bbuffer, 0, 2064);
+    i.close();
+
+    ByteArrayInputStream bi = new ByteArrayInputStream(bbuffer, 0, length);
+    Resource r = rs.createResource(URI.createURI(".xml"));
+
+    r.load(bi, Collections.EMPTY_MAP);
+  }
+}
