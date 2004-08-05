2bc4c04b4e2bb7244c38ad2365cb52722e42b207
marcelop <marcelop>
2004-08-05 14:59:57 +0000

    javadoc fix: improving the groups of packages.

--- doc/org.eclipse.emf.doc/build/antJavadoc.sh
+++ doc/org.eclipse.emf.doc/build/antJavadoc.sh
@@ -12,30 +12,60 @@ if [ -d $destDir ]; then
 	exit
 fi
 
+function groupPackage
+{
+	plugin=$1
+	hasToken=`grep "@$plugin@" $currentPath/javadoc.xml.template`
+	if [ "x$hasToken" != "x"  ]; then
+		srcDir=$eclipseDir/plugins/$plugin/src
+		if [ -d $srcDir ]; then
+			packages=`find $srcDir -type f -name '*.java' -exec grep -e '^package .*;' {} \; | sed -e 's/^package *\(.*\);/\1/' | sort | uniq | xargs | sed -e 's/ /:/g'`
+			packages=`echo $packages | sed -e 's/\//\\\\\\//g' | sed -e 's/\./\\\\\./g'`
+		
+			sed -e "s/\@${plugin}\@/${packages}/g" $currentPath/javadoc.xml.template > javadoc.xml.template.tmp
+	
+			mv javadoc.xml.template.tmp javadoc.xml.template
+		fi
+	fi
+}
+groupPackage org.eclipse.emf.codegen
+groupPackage org.eclipse.emf.codegen.ui
+groupPackage org.eclipse.emf.codegen.ecore
+groupPackage org.eclipse.emf.codegen.ecore.ui
+groupPackage org.eclipse.emf.common
+groupPackage org.eclipse.emf.common.ui
+groupPackage org.eclipse.emf.ecore
+groupPackage org.eclipse.emf.ecore.edit
+groupPackage org.eclipse.emf.ecore.editor
+groupPackage org.eclipse.emf.ecore.change
+groupPackage org.eclipse.emf.ecore.change.edit
+groupPackage org.eclipse.emf.ecore.xmi
+groupPackage org.eclipse.emf.edit
+groupPackage org.eclipse.emf.edit.ui
+groupPackage org.eclipse.emf.mapping
+groupPackage org.eclipse.emf.mapping.ui
+groupPackage org.eclipse.emf.mapping.ecore2ecore
+groupPackage org.eclipse.emf.mapping.ecore2ecore.editor
+
 # The directory of the emf plugins in the order they were built 
-pluginDirs=`find $eclipseDir/plugins -name *.jar -printf '%T@ %p\n' | sort -n | grep org.eclipse.emf | grep -v resources.jar | grep -v org.eclipse.emf.mapping.xsd2ecore | cut -f2 -d' ' | sed -e 's/\(\/.*\)\/.*/\1/'`
+pluginDirs=`find $eclipseDir/plugins -name *.jar -printf '%T@ %p\n' | sort -n | grep org.eclipse.emf | grep -v resources.jar | grep -v xsd | grep -v test  | grep -v org.eclipse.emf.java | grep -v sdo | cut -f2 -d' ' | sed -e 's/\(\/.*\)\/.*/\1/'`
 
 # All the jars in the pluigins directory
 classpath=`find $eclipseDir/plugins -name *.jar -print | grep -v org.eclipse.emf | grep -v org.eclipse.xsd | tr '\n' ';'`
 
-# Calculates the packagesets and the calls to copyDocFiles
+# Calculates the packagesets
 packagesets=""
-copydocfiles=""
 for pluginDir in $pluginDirs; do
 	pluginDir=`echo $pluginDir | sed -e 's/\/runtime$//g'`
 	srcDir=$pluginDir/src
 	if [ -d $srcDir ]; then
 		packagesets=$packagesets"<packageset dir=\"$srcDir\"><exclude name=\"$srcDir/**/doc-files/**\"/></packageset>"
-		copydocfiles=$copydocfiles"<copyDocFiles pluginDir=\"$pluginDir\"/>"
 	fi
 done
 
 # Replaces the token @packagesets@ in the template by the actual value
 packagesets=`echo $packagesets | sed -e 's/\//\\\\\\//g' | sed -e 's/\./\\\\\./g'`
-sed -e "s/\@packagesets\@/${packagesets}/g" $currentPath/javadoc.xml.template > javadoc.xml.template2
-# Replaces the token @copydocfiles@ in the template by the actual value
-copydocfiles=`echo $copydocfiles | sed -e 's/\//\\\\\\//g' | sed -e 's/\./\\\\\./g'`
-sed -e "s/\@copydocfiles\@/${copydocfiles}/g" $currentPath/javadoc.xml.template2 > javadoc.xml
+sed -e "s/\@packagesets\@/${packagesets}/g" $currentPath/javadoc.xml.template > javadoc.xml
 
 # Executes the ant script
 ant	-f javadoc.xml \
