a08718006a0cb6124528e5169664bf5aeff59d3f
nickb <nickb>
2004-10-21 21:53:01 +0000

    GLOBAL REPLACE: http://download1.eclipse.org  with http://download.eclipse.org" -- apparently we're not supposed to hardlink to the server over http

--- tests/org.eclipse.emf.test.core/src/org/eclipse/emf/test/core/common/util/URITest.java
+++ tests/org.eclipse.emf.test.core/src/org/eclipse/emf/test/core/common/util/URITest.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: URITest.java,v 1.7 2004/10/19 21:17:48 nickb Exp $
+ * $Id: URITest.java,v 1.8 2004/10/21 21:53:01 nickb Exp $
  */
 package org.eclipse.emf.test.core.common.util;
 
@@ -520,7 +520,7 @@ public class URITest extends TestCase
    */
   public void testFragmentAppendAndTrim()
   {
-    String base = "http://download1.eclipse.org/tools/emf/scripts/home.php";
+    String base = "http://download.eclipse.org/tools/emf/scripts/home.php";
     String fragment1 = "top";
     String fragment2 = "quicknav";
     String fragment3 = "over2";
