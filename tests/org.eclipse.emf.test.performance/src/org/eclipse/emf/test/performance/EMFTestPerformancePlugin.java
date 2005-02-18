426761818695a8b97a336c4bb3cd07df4653b0e0
marcelop <marcelop>
2005-02-18 06:47:12 +0000

    Invoking derby.

--- tests/org.eclipse.emf.test.performance/src/org/eclipse/emf/test/performance/EMFTestPerformancePlugin.java
+++ tests/org.eclipse.emf.test.performance/src/org/eclipse/emf/test/performance/EMFTestPerformancePlugin.java
@@ -12,9 +12,11 @@
  *
  * </copyright>
  *
- * $Id: EMFTestPerformancePlugin.java,v 1.11 2005/02/17 19:42:01 marcelop Exp $
+ * $Id: EMFTestPerformancePlugin.java,v 1.12 2005/02/18 06:47:12 marcelop Exp $
  */
 package org.eclipse.emf.test.performance;
+
+import java.lang.reflect.Method;
 
 import org.eclipse.core.runtime.Plugin;
 
@@ -30,15 +32,35 @@ extends Plugin
         
         try
         {
-          if (Class.forName("org.apache.derby.drda.NetworkServerControl") != null)
+          final Class derbyClass = Class.forName("org.apache.derby.drda.NetworkServerControl");
+          if (derbyClass != null)
           {
             System.out.println("*** Derby is in the classpath.");
+            
+            Thread thread = new Thread()
+            {
+              public void run()
+              {
+                try
+                {
+                  System.setProperty("derby.system.home", "/home/www-data/derby/derbyroot");
+                  Method mainMethod = derbyClass.getDeclaredMethod("main", new Class[]{String[].class});
+                  mainMethod.invoke(null, new Object[]{new String[]{"start","-h","0.0.0.0"}});
+                }
+                catch (Exception e)
+                {
+                  e.printStackTrace();
+                }
+              }
+            };
+            thread.start();
+            
             System.setProperty("test.target", "performance");
             System.setProperty("eclipse.perf.dbloc", "net://localhost/;dbuser=app;dbpasswd=app");
-            System.setProperty("eclipse.perf.config" ,"build=fromJava");
+            System.setProperty("eclipse.perf.config" ,"build=fromJava");            
           }
         }
-        catch (ClassNotFoundException e)
+        catch (Exception e)
         {
         }
     }
