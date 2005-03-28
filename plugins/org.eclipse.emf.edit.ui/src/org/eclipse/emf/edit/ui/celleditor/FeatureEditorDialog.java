76ec804bdd4765413bf56ab313a45b3edbdb95b6
marcelop <marcelop>
2005-03-28 20:41:18 +0000

    [89279] The list with the options is now in the left side.

--- plugins/org.eclipse.emf.edit.ui/src/org/eclipse/emf/edit/ui/celleditor/FeatureEditorDialog.java
+++ plugins/org.eclipse.emf.edit.ui/src/org/eclipse/emf/edit/ui/celleditor/FeatureEditorDialog.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: FeatureEditorDialog.java,v 1.5 2004/10/08 20:21:35 marcelop Exp $
+ * $Id: FeatureEditorDialog.java,v 1.6 2005/03/28 20:41:18 marcelop Exp $
  */
 package org.eclipse.emf.edit.ui.celleditor;
 
@@ -128,86 +128,6 @@ public class FeatureEditorDialog extends Dialog
     contentsGridData.horizontalAlignment = SWT.FILL;
     contentsGridData.verticalAlignment = SWT.FILL;
 
-    Composite featureComposite = new Composite(contents, SWT.NONE);
-    {
-      GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-      data.horizontalAlignment = SWT.END;
-      featureComposite.setLayoutData(data);
-
-      GridLayout layout = new GridLayout();
-      data.horizontalAlignment = SWT.FILL;
-      layout.marginHeight = 0;
-      layout.marginWidth = 0;
-      layout.numColumns = 1;
-      featureComposite.setLayout(layout);
-    }
-
-    Label featureLabel = new Label(featureComposite,SWT.NONE);
-    featureLabel.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Feature_label"));
-    GridData featureLabelGridData = new GridData();
-    featureLabelGridData.horizontalSpan = 2;
-    featureLabelGridData.horizontalAlignment = SWT.FILL;
-    featureLabelGridData.verticalAlignment = SWT.FILL;
-    featureLabel.setLayoutData(featureLabelGridData);
-    
-    final Table featureTable = new Table(featureComposite, SWT.MULTI | SWT.BORDER);
-    GridData featureTableGridData = new GridData();
-    featureTableGridData.widthHint = Display.getCurrent().getBounds().width / 5;
-    featureTableGridData.heightHint = Display.getCurrent().getBounds().height / 3;
-    featureTableGridData.verticalAlignment = SWT.FILL;
-    featureTableGridData.horizontalAlignment = SWT.FILL;
-    featureTableGridData.grabExcessHorizontalSpace= true;
-    featureTableGridData.grabExcessVerticalSpace= true;
-    featureTable.setLayoutData(featureTableGridData);
-
-    final TableViewer featureTableViewer = new TableViewer(featureTable);
-    featureTableViewer.setContentProvider(contentProvider);
-    featureTableViewer.setLabelProvider(labelProvider);
-    featureTableViewer.setInput(values);
-    if (!values.getChildren().isEmpty())
-    {
-      featureTableViewer.setSelection(new StructuredSelection(values.getChildren().get(0)));
-    }
-
-    Composite controlButtons = new Composite(contents, SWT.NONE);
-    GridData controlButtonsGridData = new GridData();
-    controlButtonsGridData.verticalAlignment = SWT.FILL;
-    controlButtonsGridData.horizontalAlignment = SWT.FILL;
-    controlButtons.setLayoutData(controlButtonsGridData);
-
-    GridLayout controlsButtonGridLayout = new GridLayout();
-    controlButtons.setLayout(controlsButtonGridLayout);
-
-    new Label(controlButtons, SWT.NONE);
-    
-    final Button upButton = new Button(controlButtons, SWT.PUSH);
-    upButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Up_label"));
-    GridData upButtonGridData = new GridData();
-    upButtonGridData.verticalAlignment = SWT.FILL;
-    upButtonGridData.horizontalAlignment = SWT.FILL;
-    upButton.setLayoutData(upButtonGridData);
-
-    final Button downButton = new Button(controlButtons, SWT.PUSH);
-    downButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Down_label"));
-    GridData downButtonGridData = new GridData();
-    downButtonGridData.verticalAlignment = SWT.FILL;
-    downButtonGridData.horizontalAlignment = SWT.FILL;
-    downButton.setLayoutData(downButtonGridData);
-
-    final Button addButton = new Button(controlButtons, SWT.PUSH);
-    addButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Add_label"));
-    GridData addButtonGridData = new GridData();
-    addButtonGridData.verticalAlignment = SWT.FILL;
-    addButtonGridData.horizontalAlignment = SWT.FILL;
-    addButton.setLayoutData(addButtonGridData);
-
-    final Button removeButton = new Button(controlButtons, SWT.PUSH);
-    removeButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Remove_label"));
-    GridData removeButtonGridData = new GridData();
-    removeButtonGridData.verticalAlignment = SWT.FILL;
-    removeButtonGridData.horizontalAlignment = SWT.FILL;
-    removeButton.setLayoutData(removeButtonGridData);
-
     Composite choiceComposite = new Composite(contents, SWT.NONE);
     {
       GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
@@ -262,139 +182,226 @@ public class FeatureEditorDialog extends Dialog
       choiceTextGridData.horizontalAlignment = SWT.FILL;
       choiceTextGridData.grabExcessHorizontalSpace= true;
       choiceText.setLayoutData(choiceTextGridData);
-
-      choiceText.addKeyListener
-        (new KeyAdapter()
-         {
-           public void keyPressed(KeyEvent event)
-           {
-             if (event.character == '\r' || event.character == '\n')
-             {
-               try
-               {
-                 Object value = EcoreUtil.createFromString((EDataType)eClassifier, choiceText.getText());
-                 values.getChildren().add(value);
-                 choiceText.setText("");
-                 featureTableViewer.setSelection(new StructuredSelection(value));
-                 event.doit = false;
-               }
-               catch (RuntimeException exception)
-               {
-               }
-             } 
-             else if (event.character == '\33') 
-             {
-               choiceText.setText("");
-               event.doit = false;
-             }
-           }
-         });
     }
 
-    upButton.addSelectionListener
-      (new SelectionAdapter()
-       {
-         public void widgetSelected(SelectionEvent event)
-         {
-           IStructuredSelection selection = (IStructuredSelection)featureTableViewer.getSelection();
-           int minIndex = 0;
-           for (Iterator i = selection.iterator(); i.hasNext(); )
-           {
-             Object value = i.next();
-             int index = values.getChildren().indexOf(value);
-             values.getChildren().move(Math.max(index - 1, minIndex++), value);
-           }
-         }
-       });
+    Composite controlButtons = new Composite(contents, SWT.NONE);
+    GridData controlButtonsGridData = new GridData();
+    controlButtonsGridData.verticalAlignment = SWT.FILL;
+    controlButtonsGridData.horizontalAlignment = SWT.FILL;
+    controlButtons.setLayoutData(controlButtonsGridData);
 
-    downButton.addSelectionListener
-      (new SelectionAdapter()
-       {
-         public void widgetSelected(SelectionEvent event)
-         {
-           IStructuredSelection selection = (IStructuredSelection)featureTableViewer.getSelection();
-           int maxIndex = values.getChildren().size() - selection.size();
-           for (Iterator i = selection.iterator(); i.hasNext(); )
-           {
-             Object value = i.next();
-             int index = values.getChildren().indexOf(value);
-             values.getChildren().move(Math.min(index + 1, maxIndex++), value);
-           }
-         }
-       });
+    GridLayout controlsButtonGridLayout = new GridLayout();
+    controlButtons.setLayout(controlsButtonGridLayout);
 
-    addButton.addSelectionListener
-      (new SelectionAdapter()
-       {
-         public void widgetSelected(SelectionEvent event)
-         {
-           if (choiceTableViewer != null)
-           {
-             IStructuredSelection selection = (IStructuredSelection)choiceTableViewer.getSelection();
-             for (Iterator i = selection.iterator(); i.hasNext(); )
-             {
-               Object value = i.next();
-               if (!values.getChildren().contains(value))
-               {
-                 values.getChildren().add(value);
-               }
-             }
-             featureTableViewer.setSelection(selection);
-           }
-           else
-           {
-             try
-             {
-               Object value = EcoreUtil.createFromString((EDataType)eClassifier, choiceText.getText());
-               values.getChildren().add(value);
-               choiceText.setText("");
-               featureTableViewer.setSelection(new StructuredSelection(value));
-             }
-             catch (RuntimeException exception)
-             {
-             }
-           }
-         }
-       });
+    new Label(controlButtons, SWT.NONE);
+    
+    final Button addButton = new Button(controlButtons, SWT.PUSH);
+    addButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Add_label"));
+    GridData addButtonGridData = new GridData();
+    addButtonGridData.verticalAlignment = SWT.FILL;
+    addButtonGridData.horizontalAlignment = SWT.FILL;
+    addButton.setLayoutData(addButtonGridData);
 
-    removeButton.addSelectionListener
-      (new SelectionAdapter()
-       {
-         public void widgetSelected(SelectionEvent event)
-         {
-           IStructuredSelection selection = (IStructuredSelection)featureTableViewer.getSelection();
-           Object firstValue = null;
-           for (Iterator i = selection.iterator(); i.hasNext(); )
-           {
-             Object value = i.next();
-             if (firstValue == null)
-             {
-               firstValue = value;
-             }
-             values.getChildren().remove(value);
-           }
+    final Button removeButton = new Button(controlButtons, SWT.PUSH);
+    removeButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Remove_label"));
+    GridData removeButtonGridData = new GridData();
+    removeButtonGridData.verticalAlignment = SWT.FILL;
+    removeButtonGridData.horizontalAlignment = SWT.FILL;
+    removeButton.setLayoutData(removeButtonGridData);
+    
+    Label spaceLabel = new Label(controlButtons, SWT.NONE);
+    GridData spaceLabelGridData = new GridData();
+    spaceLabelGridData.verticalSpan = 2;
+    spaceLabel.setLayoutData(spaceLabelGridData);
+    
+    final Button upButton = new Button(controlButtons, SWT.PUSH);
+    upButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Up_label"));
+    GridData upButtonGridData = new GridData();
+    upButtonGridData.verticalAlignment = SWT.FILL;
+    upButtonGridData.horizontalAlignment = SWT.FILL;
+    upButton.setLayoutData(upButtonGridData);
 
-           if (!values.getChildren().isEmpty())
-           {
-             featureTableViewer.setSelection(new StructuredSelection(values.getChildren().get(0)));
-           }
+    final Button downButton = new Button(controlButtons, SWT.PUSH);
+    downButton.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Down_label"));
+    GridData downButtonGridData = new GridData();
+    downButtonGridData.verticalAlignment = SWT.FILL;
+    downButtonGridData.horizontalAlignment = SWT.FILL;
+    downButton.setLayoutData(downButtonGridData);
 
-           if (choiceTableViewer != null)
-           {
-             choiceTableViewer.setSelection(selection);
-           }
-           else
-           {
-             if (firstValue != null)
-             {
-               String value = EcoreUtil.convertToString((EDataType)eClassifier, firstValue);
-               choiceText.setText(value);
-             }
-           }
-         }
-       });
+    Composite featureComposite = new Composite(contents, SWT.NONE);
+    {
+      GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
+      data.horizontalAlignment = SWT.END;
+      featureComposite.setLayoutData(data);
 
+      GridLayout layout = new GridLayout();
+      data.horizontalAlignment = SWT.FILL;
+      layout.marginHeight = 0;
+      layout.marginWidth = 0;
+      layout.numColumns = 1;
+      featureComposite.setLayout(layout);
+    }
 
+    Label featureLabel = new Label(featureComposite,SWT.NONE);
+    featureLabel.setText(EMFEditUIPlugin.INSTANCE.getString("_UI_Feature_label"));
+    GridData featureLabelGridData = new GridData();
+    featureLabelGridData.horizontalSpan = 2;
+    featureLabelGridData.horizontalAlignment = SWT.FILL;
+    featureLabelGridData.verticalAlignment = SWT.FILL;
+    featureLabel.setLayoutData(featureLabelGridData);
+    
+    final Table featureTable = new Table(featureComposite, SWT.MULTI | SWT.BORDER);
+    GridData featureTableGridData = new GridData();
+    featureTableGridData.widthHint = Display.getCurrent().getBounds().width / 5;
+    featureTableGridData.heightHint = Display.getCurrent().getBounds().height / 3;
+    featureTableGridData.verticalAlignment = SWT.FILL;
+    featureTableGridData.horizontalAlignment = SWT.FILL;
+    featureTableGridData.grabExcessHorizontalSpace= true;
+    featureTableGridData.grabExcessVerticalSpace= true;
+    featureTable.setLayoutData(featureTableGridData);
+
+    final TableViewer featureTableViewer = new TableViewer(featureTable);
+    featureTableViewer.setContentProvider(contentProvider);
+    featureTableViewer.setLabelProvider(labelProvider);
+    featureTableViewer.setInput(values);
+    if (!values.getChildren().isEmpty())
+    {
+      featureTableViewer.setSelection(new StructuredSelection(values.getChildren().get(0)));
+    }
+    
+    if (choiceText != null)
+    {
+      choiceText.addKeyListener(
+        new KeyAdapter()
+        {
+          public void keyPressed(KeyEvent event)
+          {
+            if (event.character == '\r' || event.character == '\n')
+            {
+              try
+              {
+                Object value = EcoreUtil.createFromString((EDataType)eClassifier, choiceText.getText());
+                values.getChildren().add(value);
+                choiceText.setText("");
+                featureTableViewer.setSelection(new StructuredSelection(value));
+                event.doit = false;
+              }
+              catch (RuntimeException exception)
+              {
+              }
+            }
+            else if (event.character == '\33')
+            {
+              choiceText.setText("");
+              event.doit = false;
+            }
+          }
+        });
+    }
+        
+    upButton.addSelectionListener(
+      new SelectionAdapter()
+      {
+        public void widgetSelected(SelectionEvent event)
+        {
+          IStructuredSelection selection = (IStructuredSelection)featureTableViewer.getSelection();
+          int minIndex = 0;
+          for (Iterator i = selection.iterator(); i.hasNext();)
+          {
+            Object value = i.next();
+            int index = values.getChildren().indexOf(value);
+            values.getChildren().move(Math.max(index - 1, minIndex++), value);
+          }
+        }
+      });
+
+    downButton.addSelectionListener(
+      new SelectionAdapter()
+      {
+        public void widgetSelected(SelectionEvent event)
+        {
+          IStructuredSelection selection = (IStructuredSelection)featureTableViewer.getSelection();
+          int maxIndex = values.getChildren().size() - selection.size();
+          for (Iterator i = selection.iterator(); i.hasNext();)
+          {
+            Object value = i.next();
+            int index = values.getChildren().indexOf(value);
+            values.getChildren().move(Math.min(index + 1, maxIndex++), value);
+          }
+        }
+      });
+
+    addButton.addSelectionListener(
+      new SelectionAdapter()
+      {
+        public void widgetSelected(SelectionEvent event)
+        {
+          if (choiceTableViewer != null)
+          {
+            IStructuredSelection selection = (IStructuredSelection)choiceTableViewer.getSelection();
+            for (Iterator i = selection.iterator(); i.hasNext();)
+            {
+              Object value = i.next();
+              if (!values.getChildren().contains(value))
+              {
+                values.getChildren().add(value);
+              }
+            }
+            featureTableViewer.setSelection(selection);
+          }
+          else
+          {
+            try
+            {
+              Object value = EcoreUtil.createFromString((EDataType)eClassifier, choiceText.getText());
+              values.getChildren().add(value);
+              choiceText.setText("");
+              featureTableViewer.setSelection(new StructuredSelection(value));
+            }
+            catch (RuntimeException exception)
+            {
+            }
+          }
+        }
+      });
+
+    removeButton.addSelectionListener(
+      new SelectionAdapter()
+      {
+        public void widgetSelected(SelectionEvent event)
+        {
+          IStructuredSelection selection = (IStructuredSelection)featureTableViewer.getSelection();
+          Object firstValue = null;
+          for (Iterator i = selection.iterator(); i.hasNext();)
+          {
+            Object value = i.next();
+            if (firstValue == null)
+            {
+              firstValue = value;
+            }
+            values.getChildren().remove(value);
+          }
+
+          if (!values.getChildren().isEmpty())
+          {
+            featureTableViewer.setSelection(new StructuredSelection(values.getChildren().get(0)));
+          }
+
+          if (choiceTableViewer != null)
+          {
+            choiceTableViewer.setSelection(selection);
+          }
+          else
+          {
+            if (firstValue != null)
+            {
+              String value = EcoreUtil.convertToString((EDataType)eClassifier, firstValue);
+              choiceText.setText(value);
+            }
+          }
+        }
+      });    
+        
     return contents;
   }
 
