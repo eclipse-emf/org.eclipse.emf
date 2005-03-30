6f65c549e87871f10febff1f2ee7c2d9e71389a9
khussey <khussey>
2005-03-30 20:47:59 +0000

    [89665] Fixing typos in ActionBarContributor.javajet and FactoryClass.javajet.

--- plugins/org.eclipse.emf.codegen.ecore/src/org/eclipse/emf/codegen/ecore/templates/editor/ActionBarContributor.java
+++ plugins/org.eclipse.emf.codegen.ecore/src/org/eclipse/emf/codegen/ecore/templates/editor/ActionBarContributor.java
@@ -155,7 +155,7 @@ public class ActionBarContributor
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2004 IBM Corporation and others.
+ * Copyright (c) 2002-2005 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
