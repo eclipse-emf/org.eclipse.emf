7d90706b84e646d1d7f90bb99cd3258bdd9a8125
bportier <bportier>
2005-04-19 17:32:30 +0000

    Bidirectional Performance Tests

--- tests/org.eclipse.emf.test.performance/src/org/eclipse/emf/test/performance/sdo/accessor/StaticBidirectionalTest.java
+++ tests/org.eclipse.emf.test.performance/src/org/eclipse/emf/test/performance/sdo/accessor/StaticBidirectionalTest.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: StaticBidirectionalTest.java,v 1.5 2005/04/04 13:56:57 emerks Exp $
+ * $Id: StaticBidirectionalTest.java,v 1.6 2005/04/19 17:32:44 bportier Exp $
  */
 package org.eclipse.emf.test.performance.sdo.accessor;
 
@@ -23,21 +23,51 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.eclipse.emf.common.notify.Notifier;
-import org.eclipse.emf.ecore.sdo.EProperty;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.sdo.util.SDOUtil;
+import org.eclipse.emf.test.performance.EMFPerformanceTestCase;
 
 import com.example.sdo.library.Book;
+import com.example.sdo.library.Library;
+import com.example.sdo.library.LibraryFactory;
+import com.example.sdo.library.LibraryPackage;
 import com.example.sdo.library.Writer;
+import com.example.sdo.library.util.DoNothingAdapter;
 import commonj.sdo.DataObject;
 import commonj.sdo.Property;
 
 
-public class StaticBidirectionalTest extends DynamicBidirectionalTest
+public class StaticBidirectionalTest extends EMFPerformanceTestCase
 {
-  protected DataObject book0;
-  protected DataObject book1;
-  protected DataObject writer0;
-  protected DataObject writer1;
-  
+  protected static final int REPETITIONS_10 = 10;
+
+  protected static final int ITERATIONS_25K = 25000;
+
+  protected static final int ITERATIONS_8K = 8000;
+
+  protected LibraryFactory libFactoryInstance = LibraryFactory.eINSTANCE;
+
+  static final int NUMBER = 20000;
+
+  protected DoNothingAdapter adapter = DoNothingAdapter.INSTANCE;
+
+  DataObject lib;
+
+  static Writer[] writers;
+
+  static Book[] books;
+
+  // model
+
+  Property authorProp;
+
+  EStructuralFeature authorFeat;
+
+  Property bookProp;
+
+  EStructuralFeature bookFeat;
+
   public StaticBidirectionalTest(String name)
   {
     super(name);
@@ -49,8 +79,8 @@ public class StaticBidirectionalTest extends DynamicBidirectionalTest
 
     testSuite.addTest(new StaticBidirectionalTest("setAdaptedByGenerated").setWarmUp(0).setRepetitions(REPETITIONS_10));
     // TODO tune warmup
-    testSuite.addTest(new StaticBidirectionalTest("setByGenerated").setWarmUp(1000).setRepetitions(REPETITIONS_10));
-    testSuite.addTest(new StaticBidirectionalTest("setWithESet").setWarmUp(1000).setRepetitions(REPETITIONS_10));
+    testSuite.addTest(new StaticBidirectionalTest("setByGenerated").setWarmUp(10).setRepetitions(REPETITIONS_10));
+    testSuite.addTest(new StaticBidirectionalTest("setWithESet").setWarmUp(10).setRepetitions(REPETITIONS_10));
 
     return testSuite;
   }
@@ -58,66 +88,73 @@ public class StaticBidirectionalTest extends DynamicBidirectionalTest
   protected void setUp() throws Exception
   {
     super.setUp();
+    libSetUp();
   }
 
-  protected void libSetup()
+  protected void libSetUp()
   {
     initLib();
-
-    //serializeDataGraph();
-
     initModel();
   }
 
   protected void initLib()
   {
-    //SDOFactory sdoFactoryInstance = SDOFactory.eINSTANCE;
-    //EDataGraph dataGraph = sdoFactoryInstance.createEDataGraph();
+    Library lib = libFactoryInstance.createLibrary();
+    this.lib = (DataObject)lib;
+    lib.setName("The Open Library");
 
-    // DocumentRoot root = ipoFactoryInstance.createDocumentRoot();
+    if (writers == null)
+    {
+      Writer writer = null;
+      writers = new Writer [NUMBER];
+      for (int i = 0; i < NUMBER; i++)
+      {
+        writer = libFactoryInstance.createWriter();
+        writers[i] = writer;
+        writer.setName("Mr. writer " + i);
+      }
+    }
 
-    //Library lib = libFactoryInstance.createLibrary();
-    //lib.setName("The Open Library");
+    List libWriters = lib.getWriters();
+    libWriters.add(0, writers[0]);
+    libWriters.add(1, writers[1]);
 
-    Writer writer = libFactoryInstance.createWriter();
-    this.writer0 = (DataObject)writer;
-    writer.setName("Mr. new writer 0");
+    if (books == null)
+    {
+      Book book = null;
+      books = new Book [NUMBER];
+      for (int i = 0; i < NUMBER; i++)
+      {
+        book = libFactoryInstance.createBook();
+        books[i] = book;
+        book.setTitle("The Title " + i);
+        book.setAuthor((Writer)writers[i]);
+      }
+    }
 
-    Book book = libFactoryInstance.createBook();
-    this.book0 = (DataObject)book;
-    book.setTitle("The New Title 0");
-    book.setAuthor(writer);
-
-    writer = libFactoryInstance.createWriter();
-    this.writer1 = (DataObject)writer;
-    writer.setName("Mr. new writer 1");
-
-    book = libFactoryInstance.createBook();
-    this.book1 = (DataObject)book;
-    book.setTitle("The New Title 1");
-    book.setAuthor(writer);
-
-    // root.setPurchaseOrder(po);
-    //dataGraph.setERootObject((EObject)root);
-    //dataGraph.setEChangeSummary(sdoFactoryInstance.createEChangeSummary());
+    List libBooks = lib.getBooks();
+    libBooks.add(0, books[0]);
+    libBooks.add(1, books[1]);
   }
 
   private void initModel()
   {
-    List bookProperties = book1.getType().getProperties();
-    authorProp = (Property)bookProperties.get(3);
-    authorFeat = ((EProperty)authorProp).getEStructuralFeature();
+    LibraryPackage libPackageInst = LibraryPackage.eINSTANCE;
 
-    List writerProperties = writer1.getType().getProperties();
-    bookProp = (Property)writerProperties.get(1);
-    bookFeat = ((EProperty)bookProp).getEStructuralFeature();
+    authorFeat = libPackageInst.getBook_Author();
+    authorProp = SDOUtil.adaptProperty(authorFeat);
+
+    bookFeat = libPackageInst.getWriter_Books();
+    bookProp = SDOUtil.adaptProperty(bookFeat);
   }
 
   public void setByGenerated()
   {
-    Book book0 = (Book)this.book0;
-    Writer writer0 = (Writer)this.writer0;
-    Writer writer1 = (Writer)this.writer1;
+    Library lib = (Library)this.lib;
+    Book book0 = (Book)lib.getBooks().get(0);
+    List writers = lib.getWriters();
+    Writer writer0 = (Writer)writers.get(0);
+    Writer writer1 = (Writer)writers.get(1);
 
     startMeasuring();
     for (int i = 0; i < ITERATIONS_25K; i++)
@@ -130,10 +167,14 @@ public class StaticBidirectionalTest extends DynamicBidirectionalTest
 
   public void setAdaptedByGenerated()
   {
-    Writer writer0 = (Writer)this.writer0;
-    Writer writer1 = (Writer)this.writer1;
-    Book book0 = (Book)this.book0;
-    Book book1 = (Book)this.book1;
+    Library lib = (Library)this.lib;
+    List books = lib.getBooks();
+    Book book0 = (Book)lib.getBooks().get(0);
+    Book book1 = (Book)lib.getBooks().get(1);
+    List writers = lib.getWriters();
+    Writer writer0 = (Writer)writers.get(0);
+    Writer writer1 = (Writer)writers.get(1);
+
     ((Notifier)book0).eAdapters().add(adapter);
     ((Notifier)book0).eAdapters().add(adapter);
     ((Notifier)writer0).eAdapters().add(adapter);
@@ -144,6 +185,24 @@ public class StaticBidirectionalTest extends DynamicBidirectionalTest
     {
       book0.setAuthor(writer1);
       book0.setAuthor(writer0);
+    }
+    stopMeasuring();
+  }
+
+  public void setWithESet()
+  {
+    Library lib = (Library)this.lib;
+    List writers = lib.getWriters();
+    EObject book0 = (EObject)lib.getBooks().get(0);
+    Object writer0 = writers.get(0);
+    Object writer1 = writers.get(1);
+    EStructuralFeature authorFeat = this.authorFeat;
+
+    startMeasuring();
+    for (int i = 0; i < ITERATIONS_25K; i++)
+    {
+      book0.eSet(authorFeat, writer1);
+      book0.eSet(authorFeat, writer0);
     }
     stopMeasuring();
   }
