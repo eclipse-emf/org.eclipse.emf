568da0f7049aca53b3468fba8b1fe4813e80395a
khussey <khussey>
2005-05-09 14:45:45 +0000

    [94114] Removing generated Ecore2Ecore menu.

--- plugins/org.eclipse.emf.mapping.ecore2ecore.editor/src/org/eclipse/emf/mapping/ecore2ecore/presentation/Ecore2EcoreActionBarContributor.java
+++ plugins/org.eclipse.emf.mapping.ecore2ecore.editor/src/org/eclipse/emf/mapping/ecore2ecore/presentation/Ecore2EcoreActionBarContributor.java
@@ -12,116 +12,24 @@
  *
  * </copyright>
  *
- * $Id: Ecore2EcoreActionBarContributor.java,v 1.4 2005/05/06 15:03:18 khussey Exp $
+ * $Id: Ecore2EcoreActionBarContributor.java,v 1.5 2005/05/09 14:45:45 khussey Exp $
  */
 package org.eclipse.emf.mapping.ecore2ecore.presentation;
 
-
 import org.eclipse.emf.edit.ui.action.EditingDomainActionBarContributor;
-
 import org.eclipse.emf.edit.ui.action.LoadResourceAction;
-
 import org.eclipse.emf.edit.ui.action.ValidateAction;
-
-import org.eclipse.emf.common.ui.viewer.IViewerProvider;
-
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
-
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-
-import org.eclipse.jface.viewers.Viewer;
-
-import org.eclipse.ui.IEditorPart;
-
-import org.eclipse.ui.PartInitException;
-
 
 /**
  * This is the action bar contributor for the Ecore2Ecore model editor.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
- * @generated
+ * @generated NOT
  */
-public class Ecore2EcoreActionBarContributor extends EditingDomainActionBarContributor implements ISelectionChangedListener
+public class Ecore2EcoreActionBarContributor extends EditingDomainActionBarContributor
 {
   /**
-   * This keeps track of the active editor.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  protected IEditorPart activeEditorPart;
-
-  /**
-   * This keeps track of the current selection provider.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  protected ISelectionProvider selectionProvider;
-
-  /**
-   * This action opens the Properties view.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  protected IAction showPropertiesViewAction =
-    new Action(Ecore2EcoreEditorPlugin.INSTANCE.getString("_UI_ShowPropertiesView_menu_item"))
-    {
-      public void run()
-      {
-        try
-        {
-          getPage().showView("org.eclipse.ui.views.PropertySheet");
-        }
-        catch (PartInitException exception)
-        {
-          Ecore2EcoreEditorPlugin.INSTANCE.log(exception);
-        }
-      }
-    };
-
-  /**
-   * This action refreshes the viewer of the current editor if the editor
-   * implements {@link org.eclipse.emf.common.ui.viewer.IViewerProvider}.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  protected IAction refreshViewerAction =
-    new Action(Ecore2EcoreEditorPlugin.INSTANCE.getString("_UI_RefreshViewer_menu_item"))
-    {
-      public boolean isEnabled()
-      {
-        return activeEditorPart instanceof IViewerProvider;
-      }
-
-      public void run()
-      {
-        if (activeEditorPart instanceof IViewerProvider)
-        {
-          Viewer viewer = ((IViewerProvider)activeEditorPart).getViewer();
-          if (viewer != null)
-          {
-            viewer.refresh();
-          }
-        }
-      }
-    };
-
-  /**
    * This creates an instance of the contributor.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
    */
   public Ecore2EcoreActionBarContributor()
   {
@@ -131,115 +39,11 @@ public class Ecore2EcoreActionBarContributor extends EditingDomainActionBarContr
 
   /**
    * This adds Separators for editor additions to the tool bar.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
    */
   public void contributeToToolBar(IToolBarManager toolBarManager)
   {
     toolBarManager.add(new Separator("ecore2ecore-settings"));
     toolBarManager.add(new Separator("ecore2ecore-additions"));
-  }
-
-  /**
-   * This adds to the menu bar a menu and some separators for editor additions,
-   * as well as the sub-menus for object creation items.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  public void contributeToMenu(IMenuManager menuManager)
-  {
-    super.contributeToMenu(menuManager);
-
-    IMenuManager submenuManager = new MenuManager(Ecore2EcoreEditorPlugin.INSTANCE.getString("_UI_Ecore2EcoreEditor_menu"), "org.eclipse.emf.mapping.ecore2ecoreMenuID");
-    menuManager.insertAfter("additions", submenuManager);
-    submenuManager.add(new Separator("settings"));
-    submenuManager.add(new Separator("actions"));
-    submenuManager.add(new Separator("additions"));
-    submenuManager.add(new Separator("additions-end"));
-
-    // Add your contributions.
-    // Ensure that you remove @generated or mark it @generated NOT
-
-    addGlobalActions(submenuManager);
-  }
-
-  /**
-   * When the active editor changes, this remembers the change and registers with it as a selection provider.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  public void setActiveEditor(IEditorPart part)
-  {
-    super.setActiveEditor(part);
-    activeEditorPart = part;
-
-    // Switch to the new selection provider.
-    //
-    if (selectionProvider != null)
-    {
-      selectionProvider.removeSelectionChangedListener(this);
-    }
-    if (part == null)
-    {
-      selectionProvider = null;
-    }
-    else
-    {
-      selectionProvider = part.getSite().getSelectionProvider();
-      selectionProvider.addSelectionChangedListener(this);
-
-      // Fake a selection changed event to update the menus.
-      //
-      if (selectionProvider.getSelection() != null)
-      {
-        selectionChanged(new SelectionChangedEvent(selectionProvider, selectionProvider.getSelection()));
-      }
-    }
-  }
-
-  /**
-   * This implements {@link org.eclipse.jface.viewers.ISelectionChangedListener},
-   * handling {@link org.eclipse.jface.viewers.SelectionChangedEvents} by querying for the children and siblings
-   * that can be added to the selected object and updating the menus accordingly.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  public void selectionChanged(SelectionChangedEvent event)
-  {
-    // Add your contributions.
-    // Ensure that you remove @generated or mark it @generated NOT
-  }
-
-  /**
-   * This populates the pop-up menu before it appears.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  public void menuAboutToShow(IMenuManager menuManager)
-  {
-    super.menuAboutToShow(menuManager);
-  }
-
-  /**
-   * This inserts global actions before the "additions-end" separator.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  protected void addGlobalActions(IMenuManager menuManager)
-  {
-    menuManager.insertAfter("additions-end", new Separator("ui-actions"));
-    menuManager.insertAfter("ui-actions", showPropertiesViewAction);
-
-    refreshViewerAction.setEnabled(refreshViewerAction.isEnabled());		
-    menuManager.insertAfter("ui-actions", refreshViewerAction);
-
-    super.addGlobalActions(menuManager);
   }
 
 }
