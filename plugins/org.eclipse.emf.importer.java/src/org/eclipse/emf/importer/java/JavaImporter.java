2530eec4a9172385308c969adc9e74af2d5a107b
davidms <davidms>
2005-05-10 17:39:01 +0000

    [75923] Initial revision of Java model importer

---
+++ plugins/org.eclipse.emf.importer.java/src/org/eclipse/emf/importer/java/JavaImporter.java
@@ -0,0 +1,84 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: JavaImporter.java,v 1.1 2005/05/10 17:39:01 davidms Exp $
+ */
+package org.eclipse.emf.importer.java;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jdt.core.JavaCore;
+
+import org.eclipse.emf.importer.ModelImporter;
+import org.eclipse.emf.importer.java.builder.JavaEcoreBuilder;
+
+
+/**
+ * @since 2.1.0
+ */
+public class JavaImporter extends ModelImporter
+{
+  public String getID()
+  {
+    return "org.eclipse.emf.importer.java";
+  }
+
+  public boolean canImport()
+  {
+    IFile genModelFile = getGenModelFile();
+    if (genModelFile != null)
+    {
+      IProject project = genModelFile.getProject();
+      try
+      {
+        return project.hasNature(JavaCore.NATURE_ID);
+      }
+      catch (CoreException e)
+      {
+        JavaImporterPlugin.INSTANCE.log(e);
+      }
+    }
+    return false;
+  }
+
+  protected IFile getGenModelFile()
+  {
+    IPath path = getGenModelPath();
+    if (path != null)
+    {
+      return getWorkspaceRoot().getFile(path);
+    }
+    return null;
+  }
+
+  protected IStatus doComputeEPackages(IProgressMonitor progressMonitor) throws Exception
+  {
+    progressMonitor.beginTask("", 2);
+    progressMonitor.subTask(JavaImporterPlugin.INSTANCE.getString("_UI_CreatingPackages_message"));
+
+    JavaEcoreBuilder javaEcoreBuilder = new JavaEcoreBuilder(getGenModelFile(), getOriginalGenModel());
+    javaEcoreBuilder.computeEPackages(progressMonitor, this);
+    return javaEcoreBuilder.getStatus();
+  }
+  
+  protected void adjustGenModel(IProgressMonitor progressMonitor)
+  {
+    super.adjustGenModel(progressMonitor);
+    getGenModel().getForeignModel().add("@model");
+  }  
+}
---
+++ plugins/org.eclipse.emf.importer.java/src/org/eclipse/emf/importer/java/ui/JavaPackagePage.java
@@ -0,0 +1,111 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: JavaPackagePage.java,v 1.1 2005/05/10 17:39:01 davidms Exp $
+ */
+package org.eclipse.emf.importer.java.ui;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+
+import org.eclipse.emf.importer.ModelImporter;
+import org.eclipse.emf.importer.java.JavaImporter;
+import org.eclipse.emf.importer.java.JavaImporterPlugin;
+import org.eclipse.emf.importer.ui.wizard.base.ModelPackagePage;
+
+
+/**
+ * @since 2.1.0
+ */
+public class JavaPackagePage extends ModelPackagePage
+{
+  public JavaPackagePage(ModelImporter modelImporter, String pageName)
+  {
+    super(modelImporter, pageName);
+  }
+
+  public JavaImporter getJavaImporter()
+  {
+    return (JavaImporter)getModelImporter();
+  }
+
+  protected void pageActivated(final boolean firstTime)
+  {
+    if (getJavaImporter().canImport())
+    {
+      setErrorMessage(null);
+      getControl().getDisplay().asyncExec(new Runnable()
+        {
+          public void run()
+          {
+            computeEPackages();
+            filterPackagesTable(firstTime);
+            getContainer().updateButtons();
+          }
+        });
+    }
+    else
+    {
+      setErrorMessage(JavaImporterPlugin.INSTANCE.getString("_UI_JavaProjectRequired_message"));
+      setPageComplete(false);
+    }
+  }
+
+  protected void computeEPackages()
+  {
+    WorkspaceModifyOperation initializeOperation = new WorkspaceModifyOperation()
+      {
+        protected void execute(IProgressMonitor progressMonitor) throws CoreException
+        {
+          try
+          {
+            computeEPackages(progressMonitor);
+          }
+          catch (Exception e)
+          {
+            JavaImporterPlugin.INSTANCE.log(e);
+          }
+          finally
+          {
+            progressMonitor.done();
+          }
+        }
+      };
+
+    try
+    {
+      getContainer().run(false, false, initializeOperation);
+    }
+    catch (Exception exception)
+    {
+      JavaImporterPlugin.INSTANCE.log(exception);
+    }
+  }
+
+  protected void computeEPackages(IProgressMonitor progressMonitor) throws Exception
+  {
+    IStatus status = getJavaImporter().computeEPackages(progressMonitor);
+    if (!status.isOK())
+    {
+      ErrorDialog.openError(
+        getShell(),
+        JavaImporterPlugin.INSTANCE.getString("_UI_LoadProblem_title"),
+        JavaImporterPlugin.INSTANCE.getString("_UI_ProblemsEncounteredProcessingJava_message"),
+        status);
+    }
+  }
+}
