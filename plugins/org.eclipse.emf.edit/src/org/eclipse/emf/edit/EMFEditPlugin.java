4ea8b803f2df13ebc7214028fb2a9e90c9ac6877
emerks <emerks>
2005-05-16 18:13:45 +0000

    [86623]  Read entries for itemProviderAdapterFactories extension point.

--- plugins/org.eclipse.emf.edit/src/org/eclipse/emf/edit/EMFEditPlugin.java
+++ plugins/org.eclipse.emf.edit/src/org/eclipse/emf/edit/EMFEditPlugin.java
@@ -12,13 +12,25 @@
  *
  * </copyright>
  *
- * $Id: EMFEditPlugin.java,v 1.3 2004/07/29 13:33:00 marcelop Exp $
+ * $Id: EMFEditPlugin.java,v 1.4 2005/05/16 18:13:45 emerks Exp $
  */
 package org.eclipse.emf.edit;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.plugin.RegistryReader;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 
 
 /**
@@ -60,7 +72,106 @@ public final class EMFEditPlugin extends EMFPlugin
   {
     return plugin;
   }
-
+  
+  /**
+   * Returns a populated instance of an {@link ComposedAdapterFactory.Descriptor.Registry item provider adapter factory registry}.
+   * @return a populated instance of an item provider adapter factory registry.
+   */
+  public static ComposedAdapterFactory.Descriptor.Registry getComposedAdapterFactoryDescriptorRegistry()
+  {
+    final ComposedAdapterFactory.Descriptor.Registry.Impl result =  
+      new ComposedAdapterFactory.Descriptor.Registry.Impl(null)
+      {
+        public ComposedAdapterFactory.Descriptor delegatedGetDescriptor(Collection types)
+        {
+          List stringTypes = new ArrayList(types.size());
+          for (Iterator i = types.iterator(); i.hasNext(); )
+          {
+            Object key = i.next();
+            if (key instanceof EPackage)
+            {
+              stringTypes.add(((EPackage)key).getNsURI());
+            }
+            else if (key instanceof Package)
+            {
+              stringTypes.add(((Package)key).getName());
+            }
+            else if (key instanceof Class)
+            {
+              stringTypes.add(((Class)key).getName());
+            }
+            else
+            {
+              return null;
+            }
+          }
+          ComposedAdapterFactory.Descriptor descriptor = (ComposedAdapterFactory.Descriptor)get(stringTypes);
+          if (descriptor != null)
+          {
+            put(types, descriptor);
+            return descriptor;
+          }
+          
+          return super.delegatedGetDescriptor(types);
+        }
+      };
+    if (INSTANCE.getPluginResourceLocator() instanceof EclipsePlugin)
+    {
+      RegistryReader registryReader = 
+         new RegistryReader(Platform.getExtensionRegistry(), INSTANCE.getSymbolicName(), "itemProviderAdapterFactories")
+         {
+           protected boolean readElement(IConfigurationElement element)
+           {
+             if (element.getName().equals("factory"))
+             {
+               String packageURI = element.getAttribute("uri");
+               String className = element.getAttribute("class");
+               String supportedTypes = element.getAttribute("supportedTypes");
+               if (packageURI == null)
+               {
+                 logMissingAttribute(element, "uri");
+               }
+               else if (className == null)
+               {
+                 logMissingAttribute(element, "class");
+               }
+               else if (supportedTypes == null)
+               {
+                 logMissingAttribute(element, "supportedTypes");
+               }
+               
+               class PluginAdapterFactoryDescriptor extends PluginClassDescriptor implements ComposedAdapterFactory.Descriptor
+               {
+                 public PluginAdapterFactoryDescriptor(IConfigurationElement element, String attributeName)
+                 {
+                   super(element, attributeName);
+                 }
+                 
+                 public AdapterFactory createAdapterFactory()
+                 {
+                   return (AdapterFactory)createInstance();
+                 }
+               }
+               
+               for (StringTokenizer stringTokenizer = new StringTokenizer(supportedTypes); stringTokenizer.hasMoreTokens(); )
+               {
+                 String supportedType = stringTokenizer.nextToken();
+                 List key = new ArrayList();
+                 key.add(packageURI);
+                 key.add(supportedType);
+                 result.put(key, new PluginAdapterFactoryDescriptor(element, "class"));
+               }
+               
+               return true;
+             }
+             return false;
+           }
+         };
+      registryReader.readRegistry();
+    }
+    return result;
+  }
+  
   /**
    * Returns the singleton instance of the Eclipse plugin.
    * @return the singleton instance.
