3cd0e347aab9575ac359a52ac71bb97e552f4a93
marcelop <marcelop>
2005-05-16 14:21:51 +0000

    [75923] Since the load button is defined in the base ModelDetailPage, there is no reason to have a specialized details page for the EcoreImporter.

--- plugins/org.eclipse.emf.importer.ecore/src/org/eclipse/emf/importer/ecore/ui/EcoreImporterWizard.java
+++ plugins/org.eclipse.emf.importer.ecore/src/org/eclipse/emf/importer/ecore/ui/EcoreImporterWizard.java
@@ -12,12 +12,14 @@
  *
  * </copyright>
  *
- * $Id: EcoreImporterWizard.java,v 1.2 2005/05/12 17:09:31 marcelop Exp $
+ * $Id: EcoreImporterWizard.java,v 1.3 2005/05/16 14:21:51 marcelop Exp $
  */
 package org.eclipse.emf.importer.ecore.ui;
 
 import org.eclipse.emf.importer.ModelImporter;
 import org.eclipse.emf.importer.ecore.EcoreImporter;
+import org.eclipse.emf.importer.ecore.EcoreImporterPlugin;
+import org.eclipse.emf.importer.ui.contribution.base.ModelDetailPage;
 import org.eclipse.emf.importer.ui.contribution.base.ModelImporterWizard;
 import org.eclipse.emf.importer.ui.contribution.base.ModelPackagePage;
 
@@ -34,8 +36,11 @@ public class EcoreImporterWizard extends ModelImporterWizard
 
   public void addPages()
   {
-    EcoreDetailPage detailPage = new EcoreDetailPage(getModelImporter(), "EcoreModel");
+    ModelDetailPage detailPage = new ModelDetailPage(getModelImporter(), "EcoreModel");
     detailPage.setShowGenModel(getModelImporter().getGenModelFileName() == null);
+    detailPage.setTitle(EcoreImporterPlugin.INSTANCE.getString("_UI_EcoreImport_title"));
+    detailPage.setDescription(EcoreImporterPlugin.INSTANCE.getString(detailPage.showGenModel()
+      ? "_UI_EcoreImportFile_description" : "_UI_EcoreImportNewProject_description"));    
     addPage(detailPage);
 
     ModelPackagePage packagePage = new ModelPackagePage(getModelImporter(), "EcorePackages");
