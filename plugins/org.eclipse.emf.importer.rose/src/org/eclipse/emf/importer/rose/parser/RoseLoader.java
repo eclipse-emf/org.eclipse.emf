91cc68fbc4152f1e499feba5e401f4c107fa357a
marcelop <marcelop>
2005-05-24 18:23:55 +0000

    [96481] If a cat file is not found, RoseLoader will try to locate it in bothe the worspace and plugins directory.  If it finds the file, it will use the URIConverter to create the inputstream.

--- plugins/org.eclipse.emf.importer.rose/src/org/eclipse/emf/importer/rose/parser/RoseLoader.java
+++ plugins/org.eclipse.emf.importer.rose/src/org/eclipse/emf/importer/rose/parser/RoseLoader.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: RoseLoader.java,v 1.1 2005/05/10 17:40:33 davidms Exp $
+ * $Id: RoseLoader.java,v 1.2 2005/05/24 18:23:55 marcelop Exp $
  */
 package org.eclipse.emf.importer.rose.parser;
 
@@ -20,6 +20,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.importer.rose.RoseImporterPlugin;
 
 
 /**
@@ -39,7 +47,7 @@ public class RoseLoader extends RoseComponent
   protected int lower = 0;
   protected int upper = 100;
 
-  public RoseLoader(String fileName) throws Exception
+  public RoseLoader(String fileName, URIConverter uriConverter) throws Exception
   {
     try
     {
@@ -58,8 +66,44 @@ public class RoseLoader extends RoseComponent
     }
     catch (Exception exception)
     {
-      System.out.println(exception.getLocalizedMessage());
-      // throw e;
+      Pattern pattern = Pattern.compile(".*/([^/]*)/((src/)?model/[^/]*\\.cat)$");
+      Matcher matcher = pattern.matcher(fileName.replaceAll("\\\\", "/"));
+      if (matcher.matches())
+      {
+        String name = matcher.group(1);
+        String tail = matcher.group(2);
+        InputStream inputStream = null;
+        try
+        {
+          inputStream = uriConverter.createInputStream(URI.createPlatformResourceURI(name + "/" + tail));
+        }
+        catch (Exception resourceException)
+        {
+          try
+          {
+            inputStream = uriConverter.createInputStream(URI.createURI("platform:/plugin/" + name + "/" + tail));
+          }
+          catch (Exception pluginException)
+          {
+          }
+        }
+        if (inputStream != null)
+        {
+          bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+          valid = true;
+          length = inputStream.available();
+          currentLength = 0;
+          oldValue = lower;
+        }
+        else
+        {
+          RoseImporterPlugin.INSTANCE.log(exception);
+        }
+      }
+      else
+      {
+        RoseImporterPlugin.INSTANCE.log(exception);
+      }
     }
   }
 
