860496ea067d22282c037f77c46c9fe4f381a618
marcelop <marcelop>
2005-06-02 04:28:50 +0000

    [98046] Removing the 'test' directory - it had old scripts that were not working.

--- plugins/org.eclipse.emf.codegen.ecore/test/ecore2genmodel.bat
+++
@@ -1,3 +0,0 @@
-
-@call generate -ecore2GenModel %*
-
--- plugins/org.eclipse.emf.codegen.ecore/test/rose2ecore.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-setlocal
-if "%VABASE%"=="" set VABASE=..\..\..
-%VABASE%\jre\bin\java -Xmx900M -Djava.compiler=NONE -verify -cp %VABASE%\startup.jar org.eclipse.core.launcher.Main -data workspace -application org.eclipse.emf.codegen.ecore.rose2ecore.Rose2Ecore %*
