eedc67365a7cc7e6084a1d8487781f062eebfa3b
nickb <nickb>
2005-06-08 05:41:57 +0000

    [98874] migrate from CPL to EPL (EMF) [98875] migrate from CPL to EPL (XSD)

--- plugins/org.eclipse.emf.common/src/org/eclipse/emf/common/command/BasicCommandStack.java
+++ plugins/org.eclipse.emf.common/src/org/eclipse/emf/common/command/BasicCommandStack.java
@@ -3,16 +3,16 @@
  *
  * Copyright (c) 2002-2004 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors: 
  *   IBM - Initial API and implementation
  *
  * </copyright>
  *
- * $Id: BasicCommandStack.java,v 1.7 2005/02/04 12:17:27 emerks Exp $
+ * $Id: BasicCommandStack.java,v 1.8 2005/06/08 05:44:08 nickb Exp $
  */
 package org.eclipse.emf.common.command;
 
