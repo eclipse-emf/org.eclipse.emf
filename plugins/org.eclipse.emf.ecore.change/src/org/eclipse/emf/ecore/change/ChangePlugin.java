3169afa239e58b3aec08c7782019dd594a45f373
emerks <emerks>
2005-06-15 10:55:05 +0000

    Put in nice copyright.

--- plugins/org.eclipse.emf.ecore.change/src/org/eclipse/emf/ecore/change/ChangePlugin.java
+++ plugins/org.eclipse.emf.ecore.change/src/org/eclipse/emf/ecore/change/ChangePlugin.java
@@ -1,8 +1,18 @@
 /**
  * <copyright>
+ *
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ *
  * </copyright>
  *
- * $Id: ChangePlugin.java,v 1.1 2005/06/14 19:56:43 marcelop Exp $
+ * $Id: ChangePlugin.java,v 1.2 2005/06/15 10:55:05 emerks Exp $
  */
 package org.eclipse.emf.ecore.change;
 
