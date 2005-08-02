1153e86e2a598167b60d0dd2b7d3557577400ee7
nickb <nickb>
2005-08-02 22:52:06 +0000

    [98874] change to EPL

--- tests/org.eclipse.emf.test.sdo/src/org/eclipse/emf/test/sdo/DateTest.java
+++ tests/org.eclipse.emf.test.sdo/src/org/eclipse/emf/test/sdo/DateTest.java
@@ -5,14 +5,14 @@
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *   IBM - Initial API and implementation
  *
  * </copyright>
  *
- * $Id: DateTest.java,v 1.2 2005/06/12 14:07:57 emerks Exp $
+ * $Id: DateTest.java,v 1.3 2005/08/02 22:52:06 nickb Exp $
  */
 package org.eclipse.emf.test.sdo;
 
