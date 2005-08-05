bb2974f0c32bc50851f0f59b97b7d6ae605352ab
nickb <nickb>
2005-08-05 20:32:13 +0000

    [104723] -> 102557

---
+++ tests/org.eclipse.emf.test.edit/src/org/eclipse/emf/test/edit/AllSuites.java
@@ -0,0 +1,63 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2002-2004 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: AllSuites.java,v 1.3.2.1 2005/08/05 20:32:13 nickb Exp $
+ */
+package org.eclipse.emf.test.edit;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+public class AllSuites extends TestSuite
+{
+  private static Test[] suites = new Test []{ 
+    org.eclipse.emf.test.edit.command.AddCommandTest.suite()
+    ,org.eclipse.emf.test.edit.command.RemoveCommandTest.suite()
+    ,org.eclipse.emf.test.edit.command.SetCommandTest.suite()
+    ,org.eclipse.emf.test.edit.provider.ComposedAdapterFactoryTest.suite()
+  };
+
+  public static Test suite()
+  {
+    return new AllSuites("EMF Edit JUnit Test Suite");
+  }
+
+  public AllSuites()
+  {
+    super();
+    populateSuite();
+  }
+
+  public AllSuites(Class theClass)
+  {
+    super(theClass);
+    populateSuite();
+  }
+
+  public AllSuites(String name)
+  {
+    super(name);
+    populateSuite();
+  }
+
+  protected void populateSuite()
+  {
+    for (int i = 0; i < suites.length; i++)
+    {
+      addTest(suites[i]);
+    }
+  }
+}
