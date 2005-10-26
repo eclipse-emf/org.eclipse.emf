cbfa7b333a60a1d0d6db52d5438e6ad902663741
elena <elena>
2005-10-26 20:53:50 +0000

    [81512] Make sure we set XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE to TRUE on load

--- tests/org.eclipse.emf.test.tools/data/ant.reload/expected/library.xsds/src/org/examples/library/hr/util/HrResourceFactoryImpl.java
+++ tests/org.eclipse.emf.test.tools/data/ant.reload/expected/library.xsds/src/org/examples/library/hr/util/HrResourceFactoryImpl.java
@@ -54,6 +54,8 @@ public class HrResourceFactoryImpl extends ResourceFactoryImpl
     result.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
 
     result.getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+
+    result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
     result.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
 
     result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_LEXICAL_HANDLER, Boolean.TRUE);
