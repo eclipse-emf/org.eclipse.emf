24a0f8ac7cb93503bd8c7b355c8a33da4b77b892
marcelop <marcelop>
2005-12-14 21:41:45 +0000

    [120964] New test to validate that the genAnnotations are not lost after reloading the model.

--- tests/org.eclipse.emf.test.tools/src/org/eclipse/emf/test/tools/codegen/GenModelTest.java
+++ tests/org.eclipse.emf.test.tools/src/org/eclipse/emf/test/tools/codegen/GenModelTest.java
@@ -23,12 +23,19 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenAnnotation;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModelFactory;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 public class GenModelTest extends TestCase
 {
@@ -48,10 +55,16 @@ public class GenModelTest extends TestCase
     ts.addTest(new GenModelTest("testGetModelQualifiedPackageNames"));
     ts.addTest(new GenModelTest("testGetEditQualifiedPackageNames"));
     ts.addTest(new GenModelTest("testGetEditorQualifiedPackageNames"));
+    ts.addTest(new GenModelTest("testReloadGenAnnotations"));
     return ts;
   }
   
   protected void setUp() throws Exception
+  {
+    genModel = createGenModel();
+  }
+  
+  protected GenModel createGenModel()
   {
     List ePackages = new ArrayList();
     ePackages.add(createEPackage(0, true, false));
@@ -59,7 +72,7 @@ public class GenModelTest extends TestCase
     ePackages.add(createEPackage(2, true, true));
     ePackages.add(createEPackage(3, false, true));
     
-    genModel = GenModelFactory.eINSTANCE.createGenModel();
+    GenModel genModel = GenModelFactory.eINSTANCE.createGenModel();
     genModel.setModelDirectory("project1/f1");
     genModel.initialize(ePackages);
     genModel.setModelPluginClass("modelPluginClass.PC");
@@ -110,6 +123,7 @@ public class GenModelTest extends TestCase
       assertEquals("subEPackage3", subPackage.getEcorePackage().getName());
       subPackage.setPrefix("S3");
     }
+    return genModel;
   }
   
   protected EPackage createEPackage(int id, boolean hasClass, boolean subPackage)
@@ -297,5 +311,75 @@ public class GenModelTest extends TestCase
     assertTrue(packageNames.remove("ePackage3.subEPackage3.tests"));
 
     assertTrue(packageNames.remove("testSuiteClass"));    
+  }
+  
+  /*
+   * Bugzilla 120964
+   */
+  public void testReloadGenAnnotations() throws Exception
+  {
+    new ResourceImpl(URI.createURI("foo.bar")).getContents().add(genModel);
+    
+    GenAnnotation nestedGenAnnotation = genModel.createGenAnnotation();
+    nestedGenAnnotation.setSource("1.1s");
+    nestedGenAnnotation.getDetails().put("1.1", "one.one");
+    nestedGenAnnotation.getDetails().put("1.2", "one.two");
+    nestedGenAnnotation.getReferences().add(genModel);
+    nestedGenAnnotation.getReferences().add(genModel.getGenPackages().get(1));
+    nestedGenAnnotation.getReferences().add(EcorePackage.Literals.EATTRIBUTE);
+    
+    GenAnnotation genAnnotation = genModel.createGenAnnotation();
+    genAnnotation.setSource("1s");
+    genAnnotation.getDetails().put("1", "one");
+    genAnnotation.getDetails().put("2", "two");
+    genAnnotation.getReferences().add(genModel);
+    genAnnotation.getReferences().add(genModel.getGenPackages().get(1));
+    genAnnotation.getReferences().add(EcorePackage.Literals.EATTRIBUTE);
+
+    genAnnotation.getReferences().add(nestedGenAnnotation);
+    nestedGenAnnotation.getReferences().add(genAnnotation);
+    genAnnotation.getGenAnnotations().add(nestedGenAnnotation);
+    genModel.getGenAnnotations().add(genAnnotation);
+    
+    assertGenModelAnnotations(genModel);
+    
+    GenModel newGenModel = createGenModel();
+    ResourceSet resourceSet = new ResourceSetImpl();
+    resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("bar", new ResourceFactoryImpl());
+    resourceSet.createResource(URI.createURI("foo.bar")).getContents().add(newGenModel);
+    newGenModel.reconcile(genModel);
+    
+    assertGenModelAnnotations(genModel);
+    assertGenModelAnnotations(newGenModel);
+  }
+  
+  protected void assertGenModelAnnotations(GenModel genModel) throws Exception
+  {
+    assertEquals(1, genModel.getGenAnnotations().size());
+    
+    GenAnnotation genAnnotation = (GenAnnotation)genModel.getGenAnnotations().get(0);
+    assertEquals("1s", genAnnotation.getSource());
+    assertEquals(2, genAnnotation.getDetails().size());
+    assertEquals("one", genAnnotation.getDetails().get("1"));
+    assertEquals("two", genAnnotation.getDetails().get("2"));
+    assertEquals(4, genAnnotation.getReferences().size());
+    assertEquals(genModel, genAnnotation.getReferences().get(0));
+    assertEquals(genModel.getGenPackages().get(1), genAnnotation.getReferences().get(1));
+    assertEquals(EcorePackage.Literals.EATTRIBUTE, genAnnotation.getReferences().get(2));
+    
+    assertEquals(1, genAnnotation.getGenAnnotations().size());
+    
+    GenAnnotation nestedGenAnnotation = (GenAnnotation)genAnnotation.getGenAnnotations().get(0);
+    assertEquals("1.1s", nestedGenAnnotation.getSource());
+    assertEquals(2, nestedGenAnnotation.getDetails().size());
+    assertEquals("one.one", nestedGenAnnotation.getDetails().get("1.1"));
+    assertEquals("one.two", nestedGenAnnotation.getDetails().get("1.2"));
+    assertEquals(4, nestedGenAnnotation.getReferences().size());
+    assertEquals(genModel, nestedGenAnnotation.getReferences().get(0));
+    assertEquals(genModel.getGenPackages().get(1), nestedGenAnnotation.getReferences().get(1));
+    assertEquals(EcorePackage.Literals.EATTRIBUTE, nestedGenAnnotation.getReferences().get(2));
+    
+    assertEquals(genAnnotation, nestedGenAnnotation.getReferences().get(3));
+    assertEquals(nestedGenAnnotation, genAnnotation.getReferences().get(3));
   }
 }
