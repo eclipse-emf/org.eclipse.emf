2805e19191ac4b560048fb6e31605b140af7304b
marcelop <marcelop>
2005-12-20 05:08:30 +0000

    [121510] The packages are now also compared by their NSURIs, as decribed in the bugzilla.  The code was also refactored so to make it easier to write extended classes.

--- plugins/org.eclipse.xsd.ecore.exporter/src/org/eclipse/xsd/ecore/exporter/XSDExporter.java
+++ plugins/org.eclipse.xsd.ecore.exporter/src/org/eclipse/xsd/ecore/exporter/XSDExporter.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: XSDExporter.java,v 1.1 2005/12/14 08:13:51 marcelop Exp $
+ * $Id: XSDExporter.java,v 1.2 2005/12/20 05:08:30 marcelop Exp $
  */
 package org.eclipse.xsd.ecore.exporter;
 
@@ -34,15 +34,24 @@ import org.eclipse.xsd.XSDAnnotation;
 import org.eclipse.xsd.XSDSchema;
 import org.eclipse.xsd.XSDSchemaDirective;
 import org.eclipse.xsd.ecore.EcoreSchemaBuilder;
+import org.eclipse.xsd.ecore.MapBuilder;
 
 /**
  * @since 2.2.0
  */
 public class XSDExporter extends ModelExporter
 {
+  protected MapBuilder mapBuilder;
+  
   public String getID()
   {
     return "org.eclipse.xsd.ecore.exporter";
+  }
+  
+  public void dispose()
+  {
+    mapBuilder = null;
+    super.dispose();
   }
   
   protected String getDefaultArtifactLocation(EPackage ePackage)
@@ -58,48 +67,42 @@ public class XSDExporter extends ModelExporter
     }
     return super.doCheckEPackageArtifactLocation(location, packageName);
   }
-
-  protected void doExport(Monitor monitor, ModelExporter.ExportData exportData) throws Exception
+  
+  protected XSDSchema getSchema(EPackage ePackage)
   {
-    ResourceSet resourceSet = new ResourceSetImpl();
-    EcoreSchemaBuilder ecoreSchemaBuilder = 
-      new EcoreSchemaBuilder(genModel.getExtendedMetaData())
+    if (mapBuilder == null)
+    {
+      mapBuilder = new EcoreSchemaBuilder(genModel.getExtendedMetaData())
       {
         protected String getQualifiedPackageName(EPackage ePackage) 
         {
           return genModel.findGenPackage(ePackage).getQualifiedPackageName();
         }
       };
-    
+    }
+    return ((EcoreSchemaBuilder)mapBuilder).getSchema(ePackage);
+  }
+
+  protected void doExport(Monitor monitor, ModelExporter.ExportData exportData) throws Exception
+  {
+    ResourceSet resourceSet = new ResourceSetImpl();    
     for (Iterator i = exportData.genPackageToArtifactURI.entrySet().iterator(); i.hasNext();)
     {
       Map.Entry entry = (Map.Entry)i.next();
       GenPackage genPackage = (GenPackage)entry.getKey();
       EPackage ePackage = genPackage.getEcorePackage();
       URI schemaLocationURI = (URI)entry.getValue();
-      XSDSchema xsdSchema = ecoreSchemaBuilder.getSchema(ePackage);
+      XSDSchema xsdSchema = getSchema(ePackage);
       for (Iterator j = xsdSchema.getContents().iterator(); j.hasNext(); )
       {
         Object content = j.next();
         if (content instanceof XSDSchemaDirective)
         {
           XSDSchemaDirective xsdSchemaDirective = (XSDSchemaDirective)content;
-          String schemaLocation = xsdSchemaDirective.getSchemaLocation();
-          EPackage referencedEPackage = genModel.getExtendedMetaData().getPackage(schemaLocation);
+          EPackage referencedEPackage = genModel.getExtendedMetaData().getPackage(xsdSchemaDirective.getSchemaLocation());
           GenPackage referencedGenPackage = genModel.findGenPackage(referencedEPackage);
-          URI actualLocation = (URI)exportData.referencedGenPackagesToArtifactURI.get(referencedGenPackage);
-          if (actualLocation == null)
-          {
-            actualLocation = (URI)exportData.genPackageToArtifactURI.get(referencedGenPackage);
-          }
-          if (actualLocation != null)
-          {
-            xsdSchemaDirective.setSchemaLocation(actualLocation.toString());
-          }
-          else if (EcorePackage.eNS_URI.equals(schemaLocation))
-          {
-            xsdSchemaDirective.setSchemaLocation("platform:/plugin/org.eclipse.emf.ecore/model/Ecore.xsd");
-          }
+          URI artifactURI = getReferencedGenPackageArtifactURI(exportData, referencedGenPackage);
+          xsdSchemaDirective.setSchemaLocation(computeSchemaLocation(xsdSchemaDirective, artifactURI));
         }
         else if (!(content instanceof XSDAnnotation))
         {
@@ -110,6 +113,45 @@ public class XSDExporter extends ModelExporter
       Resource resource = resourceSet.createResource(schemaLocationURI);
       resource.getContents().add(xsdSchema);
       resource.save(null);
+    }
+  }
+  
+  protected URI getReferencedGenPackageArtifactURI(ModelExporter.ExportData exportData, GenPackage genPackage)
+  {
+    URI artifactURI = (URI)exportData.referencedGenPackagesToArtifactURI.get(genPackage);
+    if (artifactURI == null)
+    {
+      artifactURI = (URI)exportData.genPackageToArtifactURI.get(genPackage);
+      if (artifactURI == null)
+      {
+        for (Iterator i = exportData.referencedGenPackagesToArtifactURI.entrySet().iterator(); i.hasNext();)
+        {
+          Map.Entry entry = (Map.Entry)i.next();
+          GenPackage referencedGenPackage = (GenPackage)entry.getKey();
+          if (genPackage.getNSURI().equals(referencedGenPackage.getNSURI()) 
+              && genPackage.getEcorePackage().getName().equals(referencedGenPackage.getEcorePackage().getName()))
+          {
+            artifactURI = (URI)entry.getValue();
+          }
+        }
+      }
+    }
+    return artifactURI;
+  }
+  
+  protected String computeSchemaLocation(XSDSchemaDirective xsdSchemaDirective, URI artifactURI)
+  {
+    if (artifactURI != null)
+    {
+      return artifactURI.toString();
+    }
+    else if (EcorePackage.eNS_URI.equals(xsdSchemaDirective.getSchemaLocation()))
+    {
+      return "platform:/plugin/org.eclipse.emf.ecore/model/Ecore.xsd";
+    }    
+    else
+    {
+      return xsdSchemaDirective.getSchemaLocation();
     }
   }
 }
