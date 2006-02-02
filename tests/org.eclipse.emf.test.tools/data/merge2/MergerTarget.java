9957f6df8bc8c0be62ced1ccc7cf5884fb8d9378
marcelop <marcelop>
2006-02-02 18:19:57 +0000

    Adding a new merge test for the bugzilla 126175.  This is not a bug per say: a simple change on the merge rules file does the trick.

---
+++ tests/org.eclipse.emf.test.tools/data/merge2/MergerTarget.java
@@ -0,0 +1,63 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ */
+package org.eclipse.emf.test.tools.merger;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class MergerExample
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void operation1() throws Exception
+  {
+    System.out.println("Target.operation1");
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void operation2() 
+  {
+    System.out.println("Target.operation2");
+  }
+  
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void operation3() throws IllegalAccessError
+  {
+    System.out.println("Target.operation3");
+  } 
+  
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated NOT
+   */
+  public void operation5() throws IllegalMonitorStateException
+  {
+    System.out.println("Target.operation5");
+  }  
+}
