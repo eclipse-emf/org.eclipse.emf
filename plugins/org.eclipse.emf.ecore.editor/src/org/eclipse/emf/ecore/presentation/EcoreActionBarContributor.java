cf61931b9438e9e6d863dd64213f39383cc112de
emerks <emerks>
2006-02-14 19:38:04 +0000

    [127038] Support loading an Ecore resource from the registered packages.

--- plugins/org.eclipse.emf.ecore.editor/src/org/eclipse/emf/ecore/presentation/EcoreActionBarContributor.java
+++ plugins/org.eclipse.emf.ecore.editor/src/org/eclipse/emf/ecore/presentation/EcoreActionBarContributor.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2004 IBM Corporation and others.
+ * Copyright (c) 2002-2006 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,14 +12,18 @@
  *
  * </copyright>
  *
- * $Id: EcoreActionBarContributor.java,v 1.8 2006/01/23 21:18:31 davidms Exp $
+ * $Id: EcoreActionBarContributor.java,v 1.9 2006/02/14 19:38:04 emerks Exp $
  */
 package org.eclipse.emf.ecore.presentation;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IAction;
@@ -35,17 +39,40 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.Viewer;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 import org.eclipse.emf.edit.ui.action.ControlAction;
 import org.eclipse.emf.common.ui.viewer.IViewerProvider;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.common.util.URI;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.emf.edit.ui.action.CreateChildAction;
@@ -53,6 +80,7 @@ import org.eclipse.emf.edit.ui.action.CreateSiblingAction;
 import org.eclipse.emf.edit.ui.action.EditingDomainActionBarContributor;
 import org.eclipse.emf.edit.ui.action.LoadResourceAction;
 import org.eclipse.emf.edit.ui.action.ValidateAction;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
 
 
 /**
@@ -74,6 +102,125 @@ public class EcoreActionBarContributor
     protected IMenuManager createSubmenuManager()
     {
       return new MenuManager(EcoreEditorPlugin.getPlugin().getString("_UI_ReflectiveEditor_menu"), "org.eclipse.emf.ecoreMenuID");
+    }
+  }
+  
+  public static class ExtendedLoadResourceAction extends LoadResourceAction
+  {
+    public void run()
+    {
+      ExtendedLoadResourceDialog loadResourceDialog =
+        new ExtendedLoadResourceDialog
+          (PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), domain);
+    
+      loadResourceDialog.open();
+    }
+    
+    public static class ExtendedLoadResourceDialog extends LoadResourceDialog
+    {
+      public ExtendedLoadResourceDialog(Shell parent, EditingDomain domain)
+      {
+        super(parent, domain);
+      }
+
+      protected Control createDialogArea(Composite parent)
+      {
+        Composite composite = (Composite)super.createDialogArea(parent);
+        Composite buttonComposite = (Composite)composite.getChildren()[0];
+        Button browseRegisteredPackagesButton = new Button(buttonComposite, SWT.PUSH);
+        browseRegisteredPackagesButton.setText(EcoreEditorPlugin.INSTANCE.getString("_UI_BrowseRegisteredPackages_label"));
+        prepareBrowseRegisteredPackagesButton(browseRegisteredPackagesButton);
+        {
+          FormData data = new FormData();
+          Control [] children = buttonComposite.getChildren();
+          data.right = new FormAttachment(children[0], -CONTROL_OFFSET);
+          browseRegisteredPackagesButton.setLayoutData(data);
+        }
+        return composite;
+      }
+
+      protected void prepareBrowseRegisteredPackagesButton(Button browseRegisteredPackagesButton)
+      {
+        browseRegisteredPackagesButton.addSelectionListener
+          (new SelectionAdapter()
+           {
+             public void widgetSelected(SelectionEvent event)
+             {
+               RegisteredPackageDialog registeredPackageDialog = new RegisteredPackageDialog(getShell());
+               registeredPackageDialog.open();
+               Object [] result = registeredPackageDialog.getResult();
+               if (result != null)
+               {
+                 List nsURIs = Arrays.asList(result);
+                 ResourceSet resourceSet = new ResourceSetImpl();
+                 resourceSet.getURIConverter().getURIMap().putAll(EcorePlugin.computePlatformURIMap());
+                 StringBuffer uris = new StringBuffer();
+                 Map ePackageNsURItoGenModelLocationMap = EcorePlugin.getEPackageNsURIToGenModelLocationMap();
+                 for (int i = 0, length = result.length; i < length; i++)
+                 {
+                   URI location = (URI)ePackageNsURItoGenModelLocationMap.get(result[i]);
+                   Resource resource = resourceSet.getResource(location, true);
+                   EcoreUtil.resolveAll(resource);
+                 }
+                 for (Iterator i = resourceSet.getResources().iterator(); i.hasNext(); )
+                 {
+                   Resource resource = (Resource)i.next();
+                   for (TreeIterator j = 
+                          new EcoreUtil.ContentTreeIterator(resource.getContents())
+                          {
+                            protected Iterator getEObjectChildren(EObject eObject)
+                            {
+                              return 
+                                eObject instanceof EPackage ? 
+                                  ((EPackage)eObject).getESubpackages().iterator() : 
+                                    Collections.EMPTY_LIST.iterator();
+                            }
+                          };
+                        j.hasNext(); )
+                   {
+                     Object content = j.next();
+                     if (content instanceof EPackage)
+                     {
+                       EPackage ePackage = (EPackage)content;
+                       if (nsURIs.contains(ePackage.getNsURI()))
+                       {
+                         uris.append(resource.getURI());
+                         uris.append("  ");
+                        break;
+                       }
+                     }
+                   }
+                 }
+                 uriField.setText((uriField.getText() + "  " + uris.toString()).trim());
+               }
+             }
+           });      
+      }
+    }
+    
+    public static class RegisteredPackageDialog extends ElementListSelectionDialog
+    {
+      public RegisteredPackageDialog(Shell parent)
+      {
+        super
+          (parent, 
+           new LabelProvider()
+           {
+             public Image getImage(Object element)
+             {
+               return ExtendedImageRegistry.getInstance().getImage(EcoreEditPlugin.INSTANCE.getImage("full/obj16/EPackage"));
+             }
+           });
+        
+        setMultipleSelection(true);
+        setMessage(EcoreEditorPlugin.INSTANCE.getString("_UI_SelectRegisteredPackageURI"));
+        setFilter("*");
+        Map ePackageNsURItoGenModelLocationMap = EcorePlugin.getEPackageNsURIToGenModelLocationMap();
+        Object [] result = ePackageNsURItoGenModelLocationMap.keySet().toArray(new Object[ePackageNsURItoGenModelLocationMap.size()]);
+        Arrays.sort(result);
+        setElements(result);
+        setTitle(EcoreEditorPlugin.INSTANCE.getString("_UI_PackageSelection_label"));
+      }
     }
   }
 
@@ -181,11 +328,11 @@ public class EcoreActionBarContributor
    * This creates an instance of the contributor.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
+   * @generated NOT
    */
   public EcoreActionBarContributor()
   {
-    loadResourceAction = new LoadResourceAction();
+    loadResourceAction = new ExtendedLoadResourceAction();
     validateAction = new ValidateAction();
     controlAction = new ControlAction();
   }
