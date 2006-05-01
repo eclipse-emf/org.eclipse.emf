48583bb94b5b91cc53c01a6a85b137e1ee14faba
davidms <davidms>
2006-05-01 10:25:51 +0000

    [75925] Generator adapter interface

---
+++ plugins/org.eclipse.emf.codegen.ecore/src/org/eclipse/emf/codegen/ecore/generator/GeneratorAdapter.java
@@ -0,0 +1,41 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: GeneratorAdapter.java,v 1.1 2006/05/01 10:25:51 davidms Exp $
+ */
+package org.eclipse.emf.codegen.ecore.generator;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.Monitor;
+
+/**
+ * @since 2.2.0
+ */
+public interface GeneratorAdapter
+{
+  GeneratorAdapterFactory getAdapterFactory();
+  void setAdapterFactory(GeneratorAdapterFactory adapterFactory);
+
+  Collection getCanGenerateChildren(Object object, Object projectType);
+  boolean canGenerate(Object object, Object projectType);
+  
+  Collection getGenerateChildren(Object object, Object projectType);
+  Diagnostic preGenerate(Object object, Object projectType);
+  Diagnostic generate(Object object, Object projectType, Monitor monitor);
+  Diagnostic postGenerate(Object object, Object projectType);
+
+  void dispose();
+}
