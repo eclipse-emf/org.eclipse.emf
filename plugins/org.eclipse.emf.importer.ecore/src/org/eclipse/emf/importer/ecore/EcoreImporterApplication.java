ae26b5ce4225ceb3d4a81ce108005df1581f9748
marcelop <marcelop>
2006-05-10 20:19:27 +0000

    [141141] New Ecore Application and Ant task.

---
+++ plugins/org.eclipse.emf.importer.ecore/src/org/eclipse/emf/importer/ecore/EcoreImporterApplication.java
@@ -0,0 +1,187 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: EcoreImporterApplication.java,v 1.1 2006/05/10 20:19:27 marcelop Exp $
+ */
+package org.eclipse.emf.importer.ecore;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.codegen.util.CodeGenUtil;
+import org.eclipse.emf.common.util.Monitor;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.importer.ModelImporter;
+import org.eclipse.emf.importer.ModelImporterApplication;
+
+
+public class EcoreImporterApplication extends ModelImporterApplication
+{
+  public static class PackageInfo
+  {
+    public String nsURI;
+    public String base;
+    public String prefix;
+  }
+
+  protected Map nameToPackageInfo;
+
+  public EcoreImporter getEcoreImporter()
+  {
+    return (EcoreImporter)getModelImporter();
+  }
+
+  protected ModelImporter createModelImporter()
+  {
+    return new EcoreImporter();
+  }
+
+  protected StringBuffer getUsage()
+  {
+    StringBuffer result = new StringBuffer();
+    appendLine(result, "Usage: { <model.ecore> }+ [ <model.genmodel> [ -reload ] ] <OPTION>");
+    appendLine(result, "<OPTION>          ::= [ <PROJECT-OPTION> ]  [ <PATHMAP> ]");
+    appendLine(result, "                      { <PACKAGE> }+  { <REF-PACKAGE> }* { <REF-GEN-MODEL> }*");
+    appendLine(result, "                      [ <TEMPLATE-PATH> ] [ <MODEL-PLUGIN-ID> ] [ <COPYRIGHT> ]");
+    appendLine(result, "                      [ <SDO> ] [ <QUIET> ]");
+    appendLine(result, "<PROJECT-OPTION>  ::= <MODEL-PROJECT> [ <EDIT-PROJECT> ] [ <EDITOR-PROJECT> ]");
+    appendLine(result, "                      [ <TESTS-PROJECT> ]");
+    appendLine(result, "<MODEL-PROJECT>   ::= -modelProject <model-directory> <fragment-path>");
+    appendLine(result, "<EDIT-PROJECT>    ::= -editProject <edit-directory> <fragment-path>");
+    appendLine(result, "<EDITOR-PROJECT>  ::= -editorProject <editor-directory> <fragment-path>");
+    appendLine(result, "<TESTS-PROJECT>   ::= -testsProject <tests-directory> <fragment-path>");
+    appendLine(result, "<PACKAGE>         ::= -package <nsURI> [ <base> <prefix> ]");
+    appendLine(result, "<REF-GEN-MODEL>   ::= -refGenModel <model.genmodel> { <nsURI> }+");
+    appendLine(result, "<TEMPLATE-PATH>   ::= -templatePath <template-directory>");
+    appendLine(result, "<MODEL-PLUGIN-ID> ::= -modelPluginID <plugin-ID>");    
+    appendLine(result, "<COPYRIGHT>       ::= -copyright <copyright-string>");
+    appendLine(result, "<SDO>             ::= -sdo");
+    appendLine(result, "<QUIET>           ::= -quiet");
+    appendLine(result, "");
+    appendLine(result, "For example:");
+    appendLine(result, "");
+    appendLine(result, "");
+    appendLine(result, "  ecore2genmodel");
+    appendLine(result, "    ../../etools.company/model.ecore");
+    appendLine(result, "    result/model/Extended.genmodel");
+    appendLine(result, "    -modelProject result src");
+    appendLine(result, "    -editProject result.edit src");
+    appendLine(result, "    -editorProject result.editor src");
+    appendLine(result, "    -refGenModel company.genmodel http://org.sample.company");
+    return result;
+  }
+  
+  protected void processArguments(String[] arguments, int index)
+  {
+    index = processModelAndGenModelLocationArguments(arguments, index);
+    super.processArguments(arguments, index);
+  }
+
+  protected int processArgument(String[] arguments, int index)
+  {
+    if (arguments[index].equalsIgnoreCase("-package"))
+    {
+      if (nameToPackageInfo == null)
+      {
+        nameToPackageInfo = new HashMap();
+      }
+      index = processPackageInformation(arguments, index, nameToPackageInfo);
+    }
+    else
+    {
+      return super.processArgument(arguments, index);
+    }
+    return index + 1;
+  }
+
+  protected int processPackageInformation(String[] arguments, int index, Map nsURIToPackageInfo)
+  {
+    int start = index;
+    PackageInfo packageInfo = new PackageInfo();
+    if (index + 1 < arguments.length && !arguments[index + 1].startsWith("-"))
+    {
+      packageInfo.nsURI = arguments[++index];
+      if (index + 1 < arguments.length && !arguments[index + 1].startsWith("-"))
+      {
+        packageInfo.base = arguments[++index];
+        if (index + 1 < arguments.length && !arguments[index + 1].startsWith("-"))
+        {
+          packageInfo.prefix = arguments[++index];
+        }
+      }
+      if (index - start != 1 && index - start != 3)
+      {
+        throw new IllegalArgumentException("Error: Expecting either 1 or 3 arguments for " + arguments[start]);
+      }
+      else
+      {
+        nsURIToPackageInfo.put(packageInfo.nsURI, packageInfo);
+        nsURIToPackageInfo.put(packageInfo.nsURI.toLowerCase(), packageInfo);
+        return index;
+      }
+    }
+    else
+    {
+      throw new IllegalArgumentException("Error: No package name was specified for " + arguments[start]);
+    }
+  }
+
+  protected void adjustEPackages(Monitor monitor)
+  {
+    try
+    {
+      monitor.beginTask("", 2);
+      super.adjustEPackages(CodeGenUtil.createMonitor(monitor, 1));
+      
+      List ePackages = getEcoreImporter().getEPackages();
+      traverseEPackages(ePackages);
+      getEcoreImporter().adjustEPackages(CodeGenUtil.createMonitor(monitor, 1));
+    }
+    finally
+    {
+      monitor.done();
+    }
+  }
+  
+  protected void traverseEPackages(List ePackages)
+  {
+    for (Iterator i = ePackages.iterator(); i.hasNext();)
+    {
+      EPackage ePackage = (EPackage)i.next();
+      if (nameToPackageInfo != null)
+      {
+        PackageInfo packageInfo = (PackageInfo)nameToPackageInfo.get(ePackage.getNsURI());
+        if (packageInfo != null)
+        {
+          handleEPackage(ePackage, true);
+          
+          ModelImporter.EPackageImportInfo ePackageInfo = getEcoreImporter().getEPackageImportInfo(ePackage);        
+          if (ePackageInfo.getBasePackage() == null)
+          {
+            ePackageInfo.setBasePackage(packageInfo.base);
+          }
+          if (ePackageInfo.getPrefix() == null)
+          {
+            ePackageInfo.setPrefix(packageInfo.prefix);
+          }
+        }
+      }
+      
+      handleQualifiedEPackageName(ePackage);
+      traverseEPackages(ePackage.getESubpackages());
+    }    
+  }  
+}
---
+++ plugins/org.eclipse.emf.importer.ecore/tasks/org/eclipse/emf/importer/ecore/taskdefs/EcoreGeneratorTask.java
@@ -0,0 +1,75 @@
+/**
+ * <copyright> 
+ *
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: EcoreGeneratorTask.java,v 1.1 2006/05/10 20:19:27 marcelop Exp $
+ */
+package org.eclipse.emf.importer.ecore.taskdefs;
+
+import org.eclipse.emf.ant.taskdefs.codegen.ecore.GeneratorTask;
+import org.eclipse.emf.importer.ecore.EcoreImporterApplication;
+
+
+/**
+ * <p>
+ * Generates source code from models specifed in <b>Ecore</b> files.  Exposes some 
+ * functionalities available on the {@link org.eclipse.emf.importer.ecore.EcoreImporterApplication} 
+ * class - check its <tt>printUsage()</tt> method for detailed information on the arguments 
+ * you can use with this task.
+ * </p>
+ * <p>
+ * This task is supposed to be executed by a Eclipse driver with the 
+ * <b>org.eclipse.emf.ant</b> plugin.  It is neither necessary to use Ant's task
+ * <tt>TaskDef</tt> to declare this task in a script nor to change the Ant's runtime 
+ * classpath.
+ * </p>
+ * <p>
+ * The following command line will start a headless Eclipse instance and run the specified 
+ * Ant script.
+ * </p>
+ * <p>
+ * java -classpath <i>eclipseDir</i>/startup.jar org.eclipse.core.launcher.Main
+ *      -data <i>worspaceDir</i>
+ *      -application org.eclipse.ant.core.antRunner
+ *      -buildfile <i>antScript</i>
+ * </p>
+ * <p>
+ * Usage example:
+ * </p>
+ * <pre>
+ * &lt;emf.Ecore2Java model=&quot;c:/lib/model/lib.ecore&quot; 
+ *                genModel=&quot;c:/lib/emf/lib.genmodel&quot; 
+ *                modelProject=&quot;c:/lib&quot; 
+ *                modelProjectFragmentPath=&quot;src&quot;&gt;
+ *        &lt;arg line=&quot;-package http://org.library org.examples Library&quot;/&gt;
+ * &lt;/emf.Ecore2Java&gt;
+ * </pre>
+ * <pre>
+ * &lt;emf.Ecore2Java genModel=&quot;c:/lib/emf/lib.genmodel&quot; 
+ *                modelProject=&quot;c:/lib&quot; 
+ *                modelProjectFragmentPath=&quot;src&quot;&gt;
+ *        &lt;arg line=&quot;-packages http://www.example.eclipse.org/Library&quot;/&gt;
+ *        &lt;model uri=&quot;http://www.example.eclipse.org/library.ecore&quot;/&gt;
+ *        &lt;model file=&quot;c:/hr.ecore&quot;/&gt;
+ * &lt;/emf.Ecore2Java&gt;
+ * </pre>
+ * 
+ * @since 2.2.0
+ */
+public class EcoreGeneratorTask extends GeneratorTask
+{
+  protected void createGenModel(String[] arguments) throws Exception
+  {
+    new EcoreImporterApplication().run(getProgressMonitor(), arguments);
+  }
+}
