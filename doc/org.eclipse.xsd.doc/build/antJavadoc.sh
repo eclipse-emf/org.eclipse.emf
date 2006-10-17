f878b337097567013e2687e548e5f8f0cae8b1d4
nickb <nickb>
2006-10-17 21:32:10 +0000

    better logic for finding all links

--- doc/org.eclipse.xsd.doc/build/antJavadoc.sh
+++ doc/org.eclipse.xsd.doc/build/antJavadoc.sh
@@ -137,9 +137,9 @@ echo '<?xml version="1.0" encoding="UTF-8"?>' >> $trXML;
 echo '<?NLS TYPE="org.eclipse.help.toc"?>' >> $trXML;
 echo '<toc label="Reference">' >> $trXML;
 echo '  <topic label="API Reference" href="references/javadoc/overview-summary.html">' >> $trXML;
-for pluginDir in $pluginDirs; do
-	label=${pluginDir##*/}; # org.eclipse.xsd.ecore.importer.taskdefs
-	path=${label//.\/}; # org/eclipse/xsd/ecore/importer/taskdefs
+for packSum in `find $destDir -name "package-summary.html"`; do
+	path=${packSum%/package-summary.html}; path=${path#$destDir/}; # org/eclipse/xsd/ecore/importer/taskdefs
+	label=${path//\//.}; # org.eclipse.xsd.ecore.importer.taskdefs
 	echo '    <topic label="'$label'" href="references/javadoc/'$path'/package-summary.html" />' >> $trXML;
 done
 echo '  </topic>' >> $trXML;
