e4d252693de48c446fb477475fb518c6bd9a89ba
marcelop <marcelop>
2006-11-01 22:08:01 +0000

    [163140] Adjusting the header comment

--- tests/org.eclipse.emf.test.tools/src/org/eclipse/emf/test/tools/merger/AllSuites.java
+++ tests/org.eclipse.emf.test.tools/src/org/eclipse/emf/test/tools/merger/AllSuites.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2006 IBM Corporation and others.
+ * Copyright (c) 2006 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: AllSuites.java,v 1.1 2006/11/01 21:32:17 marcelop Exp $
+ * $Id: AllSuites.java,v 1.2 2006/11/01 22:08:01 marcelop Exp $
  */
 package org.eclipse.emf.test.tools.merger;
 
--- tests/org.eclipse.emf.test.tools/src/org/eclipse/emf/test/tools/merger/JMergerCompareTest.java
+++ tests/org.eclipse.emf.test.tools/src/org/eclipse/emf/test/tools/merger/JMergerCompareTest.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: JMergerCompareTest.java,v 1.2 2006/11/01 22:08:01 marcelop Exp $
+ */
 package org.eclipse.emf.test.tools.merger;
 
 import java.io.FileInputStream;
