56beb1e88f7b12e9f000c178fc6b64ebd19f82cb
davidms <davidms>
2006-11-08 22:04:16 +0000

    [163871] Add Javadoc; fix Descriptor.DelegatingRegistry.getDescriptors(String) to delegate when the map contains an empty list

--- plugins/org.eclipse.emf.codegen.ecore/src/org/eclipse/emf/codegen/ecore/generator/GeneratorAdapterFactory.java
+++ plugins/org.eclipse.emf.codegen.ecore/src/org/eclipse/emf/codegen/ecore/generator/GeneratorAdapterFactory.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: GeneratorAdapterFactory.java,v 1.1 2006/05/01 10:26:04 davidms Exp $
+ * $Id: GeneratorAdapterFactory.java,v 1.1.2.1 2006/11/08 22:04:16 davidms Exp $
  */
 package org.eclipse.emf.codegen.ecore.generator;
 
@@ -26,25 +26,99 @@ import java.util.Map;
 import org.eclipse.emf.common.notify.AdapterFactory;
 
 /**
+ * A factory for adapters that perform code generation for a {@link Generator}.
+ * 
+ * <p>A single <code>GeneratorAdapterFactory</code> is associated with an EMF or Java package, and can return an adapter
+ * that implements {@link GeneratorAdapter} for instances of any or all of the classes within that package. Multiple
+ * factories can be associated can be associated with a single package, with all of their adapters contributing
+ * functionality to code generation for an object.
+ * 
+ * <p>A type of <code>GeneratorAdapterFactory</code> is registered, via a {@link Descriptor descriptor}, against a
+ * package ID in a {@link Descriptor.Registry registry}, in order to create adapters for that package. Typically, this
+ * is done via the <code>org.eclipse.emf.codegen.ecore.generatorAdapters</code> extension point, using an
+ * <code>adapterFactory</code> element.
+ * 
+ * <p>A <code>GeneratorAdapterFactory</code> is a factory for type <code>GeneratorAdapter.class</code>; that is, its
+ * {@link org.eclipse.emf.common.notify.AdapterFactory#isFactoryForType(Object) isFactoryForType(Object)} method should
+ * return <code>true</code> for this type. However, because EMF's adapter framework supports only one adapter of a given
+ * type on any object and because each of many generator adapter factories needs to be able to have a generator on a
+ * single object, <code>GeneratorAdapter.class</code> cannot be used as the type of the adapters. Instead, each
+ * generator adapter factory should use itself as the type of its adapters.
+ *
+ * <p>This interface is typically implemented by extending either {@link AbstractGeneratorAdapterFactory} or the
+ * generated adapter factory for a package. The latter approach provides efficient, package-specific type switching for
+ * <code>createAdapter(Notifier)</code>, while the former provides simple implementations for several other required
+ * methods.
+ * 
+ * <p>There is also a generic implementation provided by the framework that can create a single adapter for just one
+ * type of object. This implementation is used under the covers for adapters registered via the above extension point
+ * with an <code>adapter</code> element.
+ * 
  * @since 2.2.0
  */
 public interface GeneratorAdapterFactory extends AdapterFactory
 {
+  /**
+   * In essence, a factory for generator adapter factories. <code>Descriptor</code>s are stored in a
+   * {@link Registry Registry} and create a particular type of {@link GeneratorAdapterFactory}, providing an additional
+   * level of indirection that allows multiple {@link Generator}s to share a single registry and supports
+   * extension-point-based registration with deferred class loading.
+   */
   public interface Descriptor
   {
+    /**
+     * Creates and returns an instance of a particular type of <code>GeneratorAdapterFactory</code>. A new instance
+     * should be returned on each invocation. 
+     */
     GeneratorAdapterFactory createAdapterFactory();
 
+    /**
+     * A registry of {@link GeneratorAdapterFactory.Descriptor Descriptor}s. Descriptors are keyed by package ID, and
+     * multiple descriptors can be associated with each package ID.
+     */
     public interface Registry
     {
+      /**
+       * The global generator adapter factory descriptor registry.
+       */
       public static final Registry INSTANCE = new DelegatingRegistry();
 
+      /**
+       * Returns the descriptors registered against the given package ID.
+       */
       Collection getDescriptors(String packageID);
+
+      /**
+       * Registers the given descriptor against the specified package ID.
+       * @return whether the descriptor was added to the collection of descriptors for that package ID.
+       */
       boolean addDescriptor(String packageID, Descriptor descriptor);
+
+      /**
+       * Removes the given descriptor from the collection registered against the given package ID.
+       * @return whether the descriptor was deregistered (this should be true if it was previously registered against
+       * that package ID).
+       */
       boolean removeDescriptor(String packageID, Descriptor descriptor);
+
+      /**
+       * Deregisters all descriptors for a given package ID.
+       * @return whether any descriptors were deregistered (this should be true if any descriptor was previously
+       * registered against it).  
+       */
       boolean removeDescriptors(String packageID);
+
+      /**
+       * Clears the registry of all descriptor registrations.
+       */
       void clear();
     }
-    
+
+    /**
+     * A simple <code>Registry</code> implementation, in which {@link #getDescriptors(String) getDescriptors(String)}
+     * can delegate to another <code>Registry</code> if no descriptors are locally registered against the given package
+     * ID. This implementation does not prevent duplicate registrations.
+     */
     public static class DelegatingRegistry implements Registry
     {
       protected Registry delegateRegistry;
@@ -62,7 +136,7 @@ public interface GeneratorAdapterFactory extends AdapterFactory
       public Collection getDescriptors(String packageID)
       {
         List descriptors = getDescriptors(packageID, false);
-        return descriptors != null ? new ArrayList(descriptors) : delegatedGetDescriptors(packageID);
+        return descriptors != null && !descriptors.isEmpty() ? new ArrayList(descriptors) : delegatedGetDescriptors(packageID);
       }
 
       protected Collection delegatedGetDescriptors(String packageID)
@@ -106,7 +180,7 @@ public interface GeneratorAdapterFactory extends AdapterFactory
         List result = (List)map.get(packageID);
         if (result == null && forceCreate)
         {
-          result = new ArrayList(); //DMS? new UniqueEList.FastCompare();
+          result = new ArrayList();
           map.put(packageID, result);
         }
         return result;
@@ -114,8 +188,28 @@ public interface GeneratorAdapterFactory extends AdapterFactory
     }
   }
 
+  /**
+   * Returns the <code>Generator</code> associated with this adapter factory.
+   * 
+   * @see #setGenerator(Generator)
+   */
   Generator getGenerator();
+
+  /**
+   * Sets the <code>Generator</code> associated with this adapter factory.
+   * 
+   * @see #getGenerator()
+   */
   void setGenerator(Generator generator);
+
+  /**
+   * Performs initialization for the given model-level input object. Typically, this involves setting
+   * {@link Generator#getOptions() options} on the associated {@link Generator}.
+   */
   void initialize(Object input);
+
+  /**
+   * Disposes this adapter factory and all of the adapters it has created.
+   */
   void dispose();
 }
