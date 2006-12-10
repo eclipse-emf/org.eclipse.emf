9329f3fa9b78781c3630588b4b2be116ba784391
emerks <emerks>
2006-12-10 14:03:24 +0000

    [164597] Support more flexible control over URI resolving and deresolving.

---
+++ plugins/org.eclipse.emf.ecore.xmi/src/org/eclipse/emf/ecore/xmi/impl/URIHandlerImpl.java
@@ -0,0 +1,57 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: URIHandlerImpl.java,v 1.1 2006/12/10 14:03:24 emerks Exp $
+ */
+
+package org.eclipse.emf.ecore.xmi.impl;
+
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+
+
+/**
+ * This is the basic implementation of the {@link XMLResource.URIHandler URIHandler} interface. 
+ * Implementations should extend this class to implement a specialized URI handler.
+ */
+public class URIHandlerImpl implements XMLResource.URIHandler
+{
+  protected URI baseURI;
+  protected boolean resolve;
+
+  public void setBaseURI(URI uri)
+  {
+    baseURI = uri;
+    resolve = uri != null && uri.isHierarchical() && !uri.isRelative();
+  }
+
+  public URI resolve(URI uri)
+  {
+    return resolve && uri.isRelative() && uri.hasRelativePath() ? uri.resolve(baseURI) : uri;
+  }
+
+  public URI deresolve(URI uri)
+  {
+    if (resolve && !uri.isRelative())
+    {
+      URI deresolvedURI = uri.deresolve(baseURI, true, true, false);
+      if (deresolvedURI.hasRelativePath())
+      {
+        uri = deresolvedURI;
+      }
+    }
+    return uri;
+  }
+}
