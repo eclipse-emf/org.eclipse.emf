fd0b4e97e1c381fad5d9cac707e736584c612232
marcelop <marcelop>
2006-12-29 18:03:52 +0000

    [169238] Updating the year in the copyright statement.

--- plugins/org.eclipse.emf.codegen/src/org/eclipse/emf/codegen/jet/IJETNature.java
+++ plugins/org.eclipse.emf.codegen/src/org/eclipse/emf/codegen/jet/IJETNature.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2004 IBM Corporation and others.
+ * Copyright (c) 2002-2006 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: IJETNature.java,v 1.5 2006/12/19 01:49:57 marcelop Exp $
+ * $Id: IJETNature.java,v 1.6 2006/12/29 18:03:52 marcelop Exp $
  */
 package org.eclipse.emf.codegen.jet;
 
