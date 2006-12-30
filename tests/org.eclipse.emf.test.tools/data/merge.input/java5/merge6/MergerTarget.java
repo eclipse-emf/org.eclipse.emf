99b7c25bd04e11cdc07a606cc3fc126d178c4005
marcelop <marcelop>
2006-12-30 04:49:16 +0000

    [169261] Adding a test to verify if the members of a nested class are being merged.

---
+++ tests/org.eclipse.emf.test.tools/data/merge.input/java5/merge6/MergerTarget.java
@@ -0,0 +1,84 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: MergerTarget.java,v 1.1 2006/12/30 04:49:16 marcelop Exp $
+ */
+package org.eclipse.example.schoollibrary.presentation;
+
+/**
+ * Target comment
+ * <!-- begin-user-doc -->
+ * Target user comment
+ * <!-- end-user-doc -->
+ * @generated
+ */
+protected class SchoollibraryEditor
+{
+  /**
+   * Target comment
+   * <!-- begin-user-doc -->
+   * Target user comment
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected Collection<Object> removedResources = new ArrayList<Object>();
+
+  /**
+   * Target comment
+   * <!-- begin-user-doc -->
+   * Target user comment
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected void handleChangedResources()
+  {
+    // Target
+  }
+
+  /**
+   * Target comment
+   * <!-- begin-user-doc -->
+   * Target user comment
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected void firePropertyChange(int action)
+  {
+    // Target
+  }
+
+  /**
+   * Target comment
+   * <!-- begin-user-doc -->
+   * Target user comment
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected void setCurrentViewerPane(ViewerPane viewerPane)
+  {
+    // Target
+  }
+  
+  /**
+   * Target comment
+   * <!-- begin-user-doc -->
+   * Target user comment
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected class ReverseAdapterFactoryContentProvider extends AdapterFactoryContentProvider
+  {
+    /**
+     * Target comment
+     * <!-- begin-user-doc -->
+     * Target user comment
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected Object [] getElements(Object object)
+    {
+      // Target nested class method
+    }
+  }
+}
