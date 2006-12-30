d86a6c8dadb99287c75c6b8a58f78730a216c61b
marcelop <marcelop>
2006-12-30 07:39:30 +0000

    [169269] Changing the shell script to overwrite the dummy topics_Reference.xml file.

--- doc/org.eclipse.xsd.doc/build/antJavadoc.sh
+++ doc/org.eclipse.xsd.doc/build/antJavadoc.sh
@@ -84,7 +84,7 @@ ant	-f javadoc.xml \
 
 # Generate topics_Reference.xml (replacement for doclet). 
 trXML=$currentPath"/../topics_Reference.xml";
-echo '<?xml version="1.0" encoding="UTF-8"?>' >> $trXML;
+echo '<?xml version="1.0" encoding="UTF-8"?>' > $trXML;
 echo '<?NLS TYPE="org.eclipse.help.toc"?>' >> $trXML;
 echo '<toc label="Reference">' >> $trXML;
 echo '  <topic label="API Reference" href="references/javadoc/overview-summary.html">' >> $trXML;
