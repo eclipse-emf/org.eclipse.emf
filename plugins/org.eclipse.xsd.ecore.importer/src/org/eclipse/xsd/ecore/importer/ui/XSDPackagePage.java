b3c25a94813a794261fecf018004368addc2eab2
marcelop <marcelop>
2007-01-04 05:25:02 +0000

    [169500] Allowing the XSD importer referenced genpackage tree to show nested genpackages.

---
+++ plugins/org.eclipse.xsd.ecore.importer/src/org/eclipse/xsd/ecore/importer/ui/XSDPackagePage.java
@@ -0,0 +1,41 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: XSDPackagePage.java,v 1.1 2007/01/04 05:25:02 marcelop Exp $
+ */
+package org.eclipse.xsd.ecore.importer.ui;
+
+import org.eclipse.emf.importer.ModelImporter;
+import org.eclipse.emf.importer.ui.contribution.base.ModelImporterPackagePage;
+
+/**
+ * @since 2.3.0
+ */
+public class XSDPackagePage extends ModelImporterPackagePage
+{
+  /**
+   * @param modelImporter
+   * @param pageName
+   */
+  public XSDPackagePage(ModelImporter modelImporter, String pageName)
+  {
+    super(modelImporter, pageName);
+  }
+
+  @Override
+  protected boolean supportsNestedPackages()
+  {
+    return true;
+  }
+}
