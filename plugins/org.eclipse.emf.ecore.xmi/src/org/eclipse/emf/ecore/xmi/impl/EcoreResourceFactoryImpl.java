a11b07dc5f58cc8d743b47b8ef11aae9aabea242
marcelop <marcelop>
2007-01-15 22:22:33 +0000

    [169308] Setting the URIHandlerImpl.PlatformAware as the default handler for saving .ecore (and .genmodel)  resources.

--- plugins/org.eclipse.emf.ecore.xmi/src/org/eclipse/emf/ecore/xmi/impl/EcoreResourceFactoryImpl.java
+++ plugins/org.eclipse.emf.ecore.xmi/src/org/eclipse/emf/ecore/xmi/impl/EcoreResourceFactoryImpl.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2006 IBM Corporation and others.
+ * Copyright (c) 2002-2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: EcoreResourceFactoryImpl.java,v 1.6 2006/12/05 20:23:28 emerks Exp $
+ * $Id: EcoreResourceFactoryImpl.java,v 1.7 2007/01/15 22:22:33 marcelop Exp $
  */
 package org.eclipse.emf.ecore.xmi.impl;
 
@@ -49,6 +49,7 @@ public class EcoreResourceFactoryImpl extends ResourceFactoryImpl
 
     result.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
     result.getDefaultSaveOptions().put(XMLResource.OPTION_LINE_WIDTH, new Integer(80));
+    result.getDefaultSaveOptions().put(XMLResource.OPTION_URI_HANDLER, new URIHandlerImpl.PlatformSchemeAware());
     return result;
   }
 }
