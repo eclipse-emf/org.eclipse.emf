7829e25bf8c1fa78199eb43fc47d4aa8aae0ae21
marcelop <marcelop>
2007-01-15 22:21:28 +0000

    [169308] Updating the copyright.

--- plugins/org.eclipse.emf.ecore.xmi/src/org/eclipse/emf/ecore/xmi/impl/URIHandlerImpl.java
+++ plugins/org.eclipse.emf.ecore.xmi/src/org/eclipse/emf/ecore/xmi/impl/URIHandlerImpl.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006-2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: URIHandlerImpl.java,v 1.2 2007/01/15 22:20:00 marcelop Exp $
+ * $Id: URIHandlerImpl.java,v 1.3 2007/01/15 22:21:28 marcelop Exp $
  */
 
 package org.eclipse.emf.ecore.xmi.impl;
