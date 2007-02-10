f2d0f6029beb0c8f5b6ea78b5c43e1d3f6e6b270
emerks <emerks>
2007-02-10 13:16:06 +0000

    [173681] Add a new test for recording the xml version and econding properly.

--- tests/org.eclipse.emf.test.xml/src/org/eclipse/emf/test/xml/AllSuites.java
+++ tests/org.eclipse.emf.test.xml/src/org/eclipse/emf/test/xml/AllSuites.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2004 IBM Corporation and others.
+ * Copyright (c) 2002-2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: AllSuites.java,v 1.9 2006/04/11 14:48:34 emerks Exp $
+ * $Id: AllSuites.java,v 1.9.2.1 2007/02/10 13:16:06 emerks Exp $
  */
 package org.eclipse.emf.test.xml;
 
@@ -34,6 +34,7 @@ public class AllSuites extends TestSuite
     ,org.eclipse.emf.test.xml.xmi.QNameTest.suite()
     ,CrossResourceReferencesTest.suite()
     ,org.eclipse.emf.test.xml.encoding.UnicodeEncodingTest.suite() 
+    ,org.eclipse.emf.test.xml.encoding.XMLHeaderTest.suite() 
     ,org.eclipse.emf.test.xml.xsdecore.Ecore2XSDTest.suite() 
     ,XSDEcoreBuilderTests.suite()
     ,org.eclipse.emf.test.xml.xsdecore.XSD2EcoreTest.suite()
