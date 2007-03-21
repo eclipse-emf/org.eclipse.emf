12afe7755c3b575cfcda56a0b6cd7a9eb6bd5748
marcelop <marcelop>
2007-03-21 14:51:15 +0000

    [178183] Test case reproducing the bug

---
+++ tests/org.eclipse.emf.test.tools/data/merge.input/java1.4/bugzilla/178183/MergerTarget.java
@@ -0,0 +1,48 @@
+package org.eclipse.emf.test.tools.merger;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public interface Example
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int b1 = 1;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int b2 = 2;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int a1 = 1;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int a2 = 2;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  int c1 = 1;
+}
