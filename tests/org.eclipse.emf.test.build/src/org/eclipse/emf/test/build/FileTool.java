91c038eddf2a9f76c5868751b2631fc92ba31cda
marcelop <marcelop>
2007-03-23 17:36:54 +0000

    [179082] Removing the 'null' related warnings by avoiding unnecessary null checks, unnecessary null assignments, and by guarding or avoiding blocks that could throw a NPE.

--- tests/org.eclipse.emf.test.build/src/org/eclipse/emf/test/build/FileTool.java
+++ tests/org.eclipse.emf.test.build/src/org/eclipse/emf/test/build/FileTool.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2006 IBM Corporation and others.
+ * Copyright (c) 2002-2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: FileTool.java,v 1.3 2006/12/28 06:58:13 marcelop Exp $
+ * $Id: FileTool.java,v 1.4 2007/03/23 17:36:54 marcelop Exp $
  */
 package org.eclipse.emf.test.build;
 
@@ -543,9 +543,9 @@ public class FileTool
               {
                 // Ignore
               }
+              System.out.println("Could not unzip: " + fileName + ". InnerZip = " + innerZipFile.getName() + ". Length: "
+                  + innerZipFile.getName().length());
             }
-            System.out.println("Could not unzip: " + fileName + ". InnerZip = " + innerZipFile.getName() + ". Lenght: "
-                + innerZipFile.getName().length());
             e.printStackTrace();
           }
         }
