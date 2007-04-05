069bb5fc85cfb01bbd9b38280e37fdc5d27816d4
marcelop <marcelop>
2007-04-05 18:32:45 +0000

    [181272] Checking the blocked pattern

---
+++ tests/org.eclipse.emf.test.tools/data/merge.input/java5/various/annotationOnBlockedMethod/MergerExpected.java
@@ -0,0 +1,19 @@
+/**
+ * @generated
+ */
+public class Example
+{
+  /**
+   * @generated
+   */
+  @Override
+  public String getNameGen()
+  {
+    return "source";
+  }
+  
+  public String getName()
+  {
+    return getNameGen().toUpperCase();
+  }
+}
---
+++ tests/org.eclipse.emf.test.tools/data/merge.input/java5/various/annotationOnBlockedMethod/MergerSource.java
@@ -0,0 +1,14 @@
+/**
+ * @generated
+ */
+public class Example
+{
+  /**
+   * @generated
+   */
+  @Override
+  public String getNameGen()
+  {
+    return "source";
+  }
+}
