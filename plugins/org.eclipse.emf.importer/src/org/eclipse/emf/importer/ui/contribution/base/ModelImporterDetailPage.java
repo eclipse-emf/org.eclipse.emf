b54ecfa5cd1847454ce9f6c292dceaabebc42552
marcelop <marcelop>
2007-04-18 20:56:37 +0000

    [182977] If the model is not valid we shouldn't adjust its packages.

--- plugins/org.eclipse.emf.importer/src/org/eclipse/emf/importer/ui/contribution/base/ModelImporterDetailPage.java
+++ plugins/org.eclipse.emf.importer/src/org/eclipse/emf/importer/ui/contribution/base/ModelImporterDetailPage.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005-2006 IBM Corporation and others.
+ * Copyright (c) 2005-2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: ModelImporterDetailPage.java,v 1.5 2006/12/28 06:53:13 marcelop Exp $
+ * $Id: ModelImporterDetailPage.java,v 1.6 2007/04/18 20:56:37 marcelop Exp $
  */
 package org.eclipse.emf.importer.ui.contribution.base;
 
@@ -404,7 +404,10 @@ public class ModelImporterDetailPage extends ModelConverterURIPage implements IM
     try
     {
       diagnostic = getModelImporter().computeEPackages(monitor);
-      getModelImporter().adjustEPackages(monitor);
+      if (diagnostic.getSeverity() == Diagnostic.OK)
+      {
+        getModelImporter().adjustEPackages(monitor);
+      }
     }
     catch (WrappedException wrappedException)
     {
