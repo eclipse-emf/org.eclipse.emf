fd19dc5c1ab4ade715707ab23a261fcc5e96a44a
emerks <emerks>
2007-05-15 22:43:44 +0000

    [164803] Generate a public NL field.

--- tests/org.eclipse.emf.test.tools/data/ant.expected/jetcompiler/Complex.java
+++ tests/org.eclipse.emf.test.tools/data/ant.expected/jetcompiler/Complex.java
@@ -13,7 +13,7 @@ public class ComplexGen
     return result;
   }
 
-  protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "public class Complex" + NL + "{" + NL + "\tpublic void main(String[] args)" + NL + "\t{";
   protected final String TEXT_2 = NL + "\t\tSystem.out.println(\"";
   protected final String TEXT_3 = "\");";
