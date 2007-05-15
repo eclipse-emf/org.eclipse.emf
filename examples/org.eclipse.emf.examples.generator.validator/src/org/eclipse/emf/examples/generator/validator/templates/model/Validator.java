6b3984d0a7ebf346fa3edc2a2b63bdd8c9cd806f
emerks <emerks>
2007-05-15 22:42:54 +0000

    [164803] Generate a public NL field.

--- examples/org.eclipse.emf.examples.generator.validator/src/org/eclipse/emf/examples/generator/validator/templates/model/Validator.java
+++ examples/org.eclipse.emf.examples.generator.validator/src/org/eclipse/emf/examples/generator/validator/templates/model/Validator.java
@@ -14,7 +14,7 @@ public class Validator
     return result;
   }
 
-  protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "";
   protected final String TEXT_2 = "/**" + NL + " * <copyright>" + NL + " * </copyright>" + NL + " *" + NL + " * ";
   protected final String TEXT_3 = "Id";
