cad2739559aec18e7930e18e6a116c60285d96ce
emerks <emerks>
2007-06-12 21:15:35 +0000

    [192218] Fix typos in Javadoc/comments and regenerate all models with the latest templates.

--- plugins/org.eclipse.emf.mapping.xsd2ecore/src/org/eclipse/emf/mapping/xsd2ecore/util/XSD2EcoreSwitch.java
+++ plugins/org.eclipse.emf.mapping.xsd2ecore/src/org/eclipse/emf/mapping/xsd2ecore/util/XSD2EcoreSwitch.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: XSD2EcoreSwitch.java,v 1.8 2007/06/12 15:06:46 emerks Exp $
+ * $Id: XSD2EcoreSwitch.java,v 1.9 2007/06/12 21:15:35 emerks Exp $
  */
 package org.eclipse.emf.mapping.xsd2ecore.util;
 
@@ -169,13 +169,13 @@ public class XSD2EcoreSwitch<T> {
   }
 
   /**
-   * Returns the result of interpretting the object as an instance of '<em>EObject</em>'.
+   * Returns the result of interpreting the object as an instance of '<em>EObject</em>'.
    * <!-- begin-user-doc -->
    * This implementation returns null;
    * returning a non-null result will terminate the switch, but this is the last case anyway.
    * <!-- end-user-doc -->
    * @param object the target of the switch.
-   * @return the result of interpretting the object as an instance of '<em>EObject</em>'.
+   * @return the result of interpreting the object as an instance of '<em>EObject</em>'.
    * @see #doSwitch(org.eclipse.emf.ecore.EObject)
    * @generated
    */
