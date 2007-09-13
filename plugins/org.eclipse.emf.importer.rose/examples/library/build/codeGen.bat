af2dee2da7ca38f6a2a2b89b525eb380ec36c02a
marcelop <marcelop>
2007-09-13 04:32:06 +0000

    [200421] Updating the readme and the example to use 'eclipsec -noSplash' instead of 'java..."

--- plugins/org.eclipse.emf.importer.rose/examples/library/build/codeGen.bat
+++ plugins/org.eclipse.emf.importer.rose/examples/library/build/codeGen.bat
@@ -8,7 +8,7 @@ rem The directory indicated by the -modelProject argument should be neither a pa
 set WORKSPACE=%MAIN_DIR%\..\codegenWorkspace
 
 rem Rose2GenModel application
-java -classpath %ECLIPSE_DIR%\startup.jar org.eclipse.core.launcher.Main -clean -data %WORKSPACE% -application org.eclipse.emf.importer.rose.Rose2GenModel %MAIN_DIR%\model\library.mdl %MAIN_DIR%\emf\library.genmodel -modelProject %MAIN_DIR% src -modelPluginID library.model -copyright "This is my code." -jdkLevel "5.0" -noQualify -package library library library.xmi org.examples Library
+%ECLIPSE_DIR%\eclipsec -noSplash -clean -data %WORKSPACE% -application org.eclipse.emf.importer.rose.Rose2GenModel %MAIN_DIR%\model\library.mdl %MAIN_DIR%\emf\library.genmodel -modelProject %MAIN_DIR% src -modelPluginID library.model -copyright "This is my code." -jdkLevel "5.0" -noQualify -package library library library.xmi org.examples Library
 
 rem Generator application
-java -classpath %ECLIPSE_DIR%\startup.jar org.eclipse.core.launcher.Main -clean -data %WORKSPACE% -application org.eclipse.emf.codegen.ecore.Generator -model %MAIN_DIR%\emf\library.genmodel
+%ECLIPSE_DIR%\eclipsec -noSplash -clean -data %WORKSPACE% -application org.eclipse.emf.codegen.ecore.Generator -model %MAIN_DIR%\emf\library.genmodel
