849b0000f3826e4670fbb7b0637f71ba833413dd
emerks <emerks>
2008-02-20 22:12:48 +0000

    [219116] Make the branches feature be proxy resolving containment and regenerate with the latest templates.

--- examples/org.eclipse.emf.examples.library.editor/src/org/eclipse/emf/examples/extlibrary/presentation/EXTLibraryEditor.java
+++ examples/org.eclipse.emf.examples.library.editor/src/org/eclipse/emf/examples/extlibrary/presentation/EXTLibraryEditor.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: EXTLibraryEditor.java,v 1.11 2007/06/12 15:06:45 emerks Exp $
+ * $Id: EXTLibraryEditor.java,v 1.12 2008/02/20 22:12:48 emerks Exp $
  */
 package org.eclipse.emf.examples.extlibrary.presentation;
 
@@ -501,94 +501,90 @@ public class EXTLibraryEditor extends MultiPageEditorPart
     {
       public void resourceChanged(IResourceChangeEvent event)
       {
-        // Only listening to these.
-        // if (event.getType() == IResourceDelta.POST_CHANGE)
+        IResourceDelta delta = event.getDelta();
+        try
         {
-          IResourceDelta delta = event.getDelta();
-          try
+          class ResourceDeltaVisitor implements IResourceDeltaVisitor
           {
-            class ResourceDeltaVisitor implements IResourceDeltaVisitor
-            {
-              protected ResourceSet resourceSet = editingDomain.getResourceSet();
-              protected Collection<Resource> changedResources = new ArrayList<Resource>();
-              protected Collection<Resource> removedResources = new ArrayList<Resource>();
+            protected ResourceSet resourceSet = editingDomain.getResourceSet();
+            protected Collection<Resource> changedResources = new ArrayList<Resource>();
+            protected Collection<Resource> removedResources = new ArrayList<Resource>();
 
-              public boolean visit(IResourceDelta delta)
+            public boolean visit(IResourceDelta delta)
+            {
+              if (delta.getResource().getType() == IResource.FILE)
               {
-                if (delta.getFlags() != IResourceDelta.MARKERS &&
-                    delta.getResource().getType() == IResource.FILE)
+                if (delta.getKind() == IResourceDelta.REMOVED ||
+                    delta.getKind() == IResourceDelta.CHANGED && delta.getFlags() != IResourceDelta.MARKERS)
                 {
-                  if ((delta.getKind() & (IResourceDelta.CHANGED | IResourceDelta.REMOVED)) != 0)
+                  Resource resource = resourceSet.getResource(URI.createURI(delta.getFullPath().toString()), false);
+                  if (resource != null)
                   {
-                    Resource resource = resourceSet.getResource(URI.createURI(delta.getFullPath().toString()), false);
-                    if (resource != null)
+                    if (delta.getKind() == IResourceDelta.REMOVED)
                     {
-                      if ((delta.getKind() & IResourceDelta.REMOVED) != 0)
-                      {
-                        removedResources.add(resource);
-                      }
-                      else if (!savedResources.remove(resource))
-                      {
-                        changedResources.add(resource);
-                      }
+                      removedResources.add(resource);
+                    }
+                    else if (!savedResources.remove(resource))
+                    {
+                      changedResources.add(resource);
                     }
                   }
                 }
-
-                return true;
               }
 
-              public Collection<Resource> getChangedResources()
-              {
-                return changedResources;
-              }
-
-              public Collection<Resource> getRemovedResources()
-              {
-                return removedResources;
-              }
+              return true;
             }
 
-            ResourceDeltaVisitor visitor = new ResourceDeltaVisitor();
-            delta.accept(visitor);
-
-            if (!visitor.getRemovedResources().isEmpty())
+            public Collection<Resource> getChangedResources()
             {
-              removedResources.addAll(visitor.getRemovedResources());
-              if (!isDirty())
-              {
-                getSite().getShell().getDisplay().asyncExec
-                  (new Runnable()
-                   {
-                     public void run()
-                     {
-                       getSite().getPage().closeEditor(EXTLibraryEditor.this, false);
-                       EXTLibraryEditor.this.dispose();
-                     }
-                   });
-              }
+              return changedResources;
             }
 
-            if (!visitor.getChangedResources().isEmpty())
+            public Collection<Resource> getRemovedResources()
             {
-              changedResources.addAll(visitor.getChangedResources());
-              if (getSite().getPage().getActiveEditor() == EXTLibraryEditor.this)
-              {
-                getSite().getShell().getDisplay().asyncExec
-                  (new Runnable()
-                   {
-                     public void run()
-                     {
-                       handleActivate();
-                     }
-                   });
-              }
+              return removedResources;
             }
           }
-          catch (CoreException exception)
+
+          ResourceDeltaVisitor visitor = new ResourceDeltaVisitor();
+          delta.accept(visitor);
+
+          if (!visitor.getRemovedResources().isEmpty())
           {
-            EXTLibraryEditorPlugin.INSTANCE.log(exception);
+            removedResources.addAll(visitor.getRemovedResources());
+            if (!isDirty())
+            {
+              getSite().getShell().getDisplay().asyncExec
+                (new Runnable()
+                 {
+                   public void run()
+                   {
+                     getSite().getPage().closeEditor(EXTLibraryEditor.this, false);
+                     EXTLibraryEditor.this.dispose();
+                   }
+                 });
+            }
           }
+
+          if (!visitor.getChangedResources().isEmpty())
+          {
+            changedResources.addAll(visitor.getChangedResources());
+            if (getSite().getPage().getActiveEditor() == EXTLibraryEditor.this)
+            {
+              getSite().getShell().getDisplay().asyncExec
+                (new Runnable()
+                 {
+                   public void run()
+                   {
+                     handleActivate();
+                   }
+                 });
+            }
+          }
+        }
+        catch (CoreException exception)
+        {
+          EXTLibraryEditorPlugin.INSTANCE.log(exception);
         }
       }
     };
@@ -641,6 +637,10 @@ public class EXTLibraryEditor extends MultiPageEditorPart
   {
     if (!changedResources.isEmpty() && (!isDirty() || handleDirtyConflict()))
     {
+      if (isDirty())
+      {
+        changedResources.addAll(editingDomain.getResourceSet().getResources());
+      }
       editingDomain.getCommandStack().flush();
 
       updateProblemIndication = false;
@@ -662,6 +662,12 @@ public class EXTLibraryEditor extends MultiPageEditorPart
           }
         }
       }
+
+      if (AdapterFactoryEditingDomain.isStale(editorSelection))
+      {
+        setSelection(StructuredSelection.EMPTY);
+      }
+
       updateProblemIndication = true;
       updateProblemIndication();
     }
