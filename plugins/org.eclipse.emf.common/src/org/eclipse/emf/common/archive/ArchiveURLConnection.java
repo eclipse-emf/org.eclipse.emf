b09ac660b844231af93d8fc5b2e72ad90a1b290d
emerks <emerks>
2008-05-25 18:01:26 +0000

    [233861] Don't use toString on a string.

--- plugins/org.eclipse.emf.common/src/org/eclipse/emf/common/archive/ArchiveURLConnection.java
+++ plugins/org.eclipse.emf.common/src/org/eclipse/emf/common/archive/ArchiveURLConnection.java
@@ -590,7 +590,7 @@ public class ArchiveURLConnection extends URLConnection
    */
   protected OutputStream createOutputStream(String nestedURL) throws IOException
   {
-    URL url = new URL(nestedURL.toString());
+    URL url = new URL(nestedURL);
     URLConnection urlConnection = url.openConnection();
     urlConnection.setDoOutput(true);
     return urlConnection.getOutputStream(); 
