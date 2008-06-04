4e4ee20f38684b298aa647161e0fc9c68a7a275a
marcelop <marcelop>
2008-06-04 14:59:53 +0000

    [235610] New merge test that shows how to overwrite comments on target members that don't have the user-doc section

---
+++ tests/org.eclipse.emf.test.tools/data/merge.input/java1.4/various/commentsWithoutUserSection/MergerSource.java
@@ -0,0 +1,47 @@
+/**
+ * Implementation for CallbackTestInterface generated on 2008-05-28T15:36:44.125-0700 using plug-in com.ibm.pdq.tools.Generator version IBM Data Studio pureQuery Runtime 1.2 build 1.3.61
+ * <!-- begin-user-doc -->
+ * Source header user doc
+ * <!-- end-user-doc -->
+ * 
+ * @generated
+ */
+public class CallbackTestInterfaceImpl  extends BaseData implements CallbackTestInterface
+{
+  /**
+   * @generated
+   */
+  public static final String generatorVersion1 = "1.3.61";
+
+  /**
+   * source comment
+   * @generated
+   */
+  public static final String generatorVersion2 = "1.3.61";
+
+  /**
+   * source comment
+   * <!-- begin-user-doc -->
+   * Source user 1
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public static final String generatorVersion3 = "1.3.61";
+
+  /**
+   * source comment
+   * <!-- begin-user-doc -->
+   * Source user 2
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public static final String generatorVersion4 = "1.3.61";
+
+  /**
+   * source comment
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public static final String generatorVersion5 = "1.3.61";
+}
