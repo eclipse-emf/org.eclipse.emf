50a86bda0a3c2789422edfb5aacc5cb0067b59e2
marcelop <marcelop>
2008-09-27 22:06:12 +0000

    [246508] Test case for bugzilla 246508

---
+++ tests/org.eclipse.emf.test.tools/data/merge.input/java5/bugzilla/246508/MergerSource.java
@@ -0,0 +1,22 @@
+package com.castortech.CobolInterpreter.syntaxtree;
+
+/**
+ * Grammar production:
+ * <pre>
+ * f0 -> &lt;ALL&gt;
+ * </pre>
+ *
+ * @generated
+ * @unmodifiable
+ */
+public final class All extends BaseCodeNode implements IBaseCodeNode {
+  /**
+   * @generated
+   */
+  @UsedViaReflection
+  public All(final BaseCodeInterpreter interpreter) {
+    super(interpreter);
+  }
+  
+  //rest of class
+}
---
+++ tests/org.eclipse.emf.test.tools/data/merge.input/java5/bugzilla/246508/MergerTarget.java
@@ -0,0 +1,21 @@
+package com.castortech.CobolInterpreter.syntaxtree;
+
+/**
+ * Grammar production:
+ * <pre>
+ * f0 -> &lt;ALL&gt;
+ * </pre>
+ *
+ * @generated
+ * @unmodifiable
+ */
+public final class All extends BaseCodeNode implements IBaseCodeNode {
+  /**
+   * @generated
+   */
+  public All(final BaseCodeInterpreter interpreter) {
+    super(interpreter);
+  }
+  
+  //rest of class
+}
