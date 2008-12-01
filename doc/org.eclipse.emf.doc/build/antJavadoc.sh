032a5c243f7509193d596b3bfc8f01cf2ad70ec7
nickb <nickb>
2008-12-01 06:56:48 +0000

    [257000] backport from HEAD

--- doc/org.eclipse.emf.doc/build/antJavadoc.sh
+++ doc/org.eclipse.emf.doc/build/antJavadoc.sh
@@ -1,4 +1,8 @@
-# The current direcotry
+if [ "x"$ANT_HOME = "x" ]; then export ANT_HOME=/opt/apache-ant-1.6; fi
+if [ "x"$JAVA_HOME = "x" ]; then export JAVA_HOME=/opt/ibm-java2-1.4; fi
+export PATH=${PATH}:${ANT_HOME}/bin
+
+# current directory
 currentPath=$PWD
 
 # The eclipse directory
