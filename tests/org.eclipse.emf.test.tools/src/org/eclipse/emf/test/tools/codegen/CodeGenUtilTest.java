2787adcd96fe9afcb51a7780cdefb072cce7f306
davidms <davidms>
2009-04-21 19:25:56 +0000

    [273104] Move ImporterUtilTest to CodeGenUtilTests, since validPluginID() has moved

---
+++ tests/org.eclipse.emf.test.tools/src/org/eclipse/emf/test/tools/codegen/CodeGenUtilTest.java
@@ -0,0 +1,51 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2005-2009 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: CodeGenUtilTest.java,v 1.1 2009/04/21 19:25:56 davidms Exp $
+ */
+package org.eclipse.emf.test.tools.codegen;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.eclipse.emf.codegen.util.CodeGenUtil;
+
+public class CodeGenUtilTest extends TestCase
+{
+  /**
+   * @param name
+   */
+  public CodeGenUtilTest(String name)
+  {
+    super(name);
+  }
+
+  public static Test suite()
+  {
+    TestSuite ts = new TestSuite("CodeGenUtilTest");
+    ts.addTest(new CodeGenUtilTest("testValidPluginID"));
+    return ts;
+  }
+
+  public void testValidPluginID()
+  {
+    assertEquals("ab.c.12", CodeGenUtil.validPluginID("ab.c.12"));
+    assertEquals("AB-c_DE", CodeGenUtil.validPluginID("AB-c_DE"));
+    assertEquals("a_b._c._1_2", CodeGenUtil.validPluginID("a b. c. 1 2"));
+    assertEquals("a-b_.c.-1_2", CodeGenUtil.validPluginID("a-b .c.-1 2"));
+    assertEquals("a-b.c.-12", CodeGenUtil.validPluginID("a-b�.c.-1�2"));
+    assertEquals("_", CodeGenUtil.validPluginID("� �"));
+  }
+}
