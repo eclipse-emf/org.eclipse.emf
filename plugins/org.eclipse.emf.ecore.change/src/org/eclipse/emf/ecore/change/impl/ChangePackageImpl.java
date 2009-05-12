b336cf6d3e007f9abf0ae9be05d5424dfe0ae844
davidms <davidms>
2009-05-12 15:54:41 +0000

    [275476] Regen to pick up package init() change

--- plugins/org.eclipse.emf.ecore.change/src/org/eclipse/emf/ecore/change/impl/ChangePackageImpl.java
+++ plugins/org.eclipse.emf.ecore.change/src/org/eclipse/emf/ecore/change/impl/ChangePackageImpl.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: ChangePackageImpl.java,v 1.17 2009/04/18 11:38:46 emerks Exp $
+ * $Id: ChangePackageImpl.java,v 1.18 2009/05/12 15:54:41 davidms Exp $
  */
 package org.eclipse.emf.ecore.change.impl;
 
@@ -128,20 +128,10 @@ public class ChangePackageImpl extends EPackageImpl implements ChangePackage
   private static boolean isInited = false;
 
   /**
-   * Creates, registers, and initializes the <b>Package</b> for this
-   * model, and for any others upon which it depends.  Simple
-   * dependencies are satisfied by calling this method on all
-   * dependent packages before doing anything else.  This method drives
-   * initialization for interdependent packages directly, in parallel
-   * with this package, itself.
-   * <p>Of this package and its interdependencies, all packages which
-   * have not yet been registered by their URI values are first created
-   * and registered.  The packages are then initialized in two steps:
-   * meta-model objects for all of the packages are created before any
-   * are initialized, since one package's meta-model objects may refer to
-   * those of another.
-   * <p>Invocation of this method will not affect any packages that have
-   * already been initialized.
+   * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+   * 
+   * <p>This method is used to initialize {@link ChangePackage#eINSTANCE} when that field is accessed.
+   * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
    * @see #eNS_URI
@@ -170,6 +160,9 @@ public class ChangePackageImpl extends EPackageImpl implements ChangePackage
     // Mark meta-data to indicate it can't be changed
     theChangePackage.freeze();
 
+  
+    // Update the registry and return the package
+    EPackage.Registry.INSTANCE.put(ChangePackage.eNS_URI, theChangePackage);
     return theChangePackage;
   }
 
