78febdae20e80de657bdba128647bbaefade81db
davidms <davidms>
2009-05-12 15:55:25 +0000

    [275476] Update expected test results with package init() change

--- tests/org.eclipse.emf.test.tools/data/ant.expected/models/5.0/creation/library.xsds/src/org/examples/library/elements/impl/ElementsPackageImpl.java
+++ tests/org.eclipse.emf.test.tools/data/ant.expected/models/5.0/creation/library.xsds/src/org/examples/library/elements/impl/ElementsPackageImpl.java
@@ -1,7 +1,7 @@
 /**
  * This is my code.
  *
- * $Id: ElementsPackageImpl.java,v 1.6 2009/04/18 11:49:55 emerks Exp $
+ * $Id: ElementsPackageImpl.java,v 1.7 2009/05/12 15:55:25 davidms Exp $
  */
 package org.examples.library.elements.impl;
 
@@ -90,20 +90,10 @@ public class ElementsPackageImpl extends EPackageImpl implements ElementsPackage
   private static boolean isInited = false;
 
   /**
-   * Creates, registers, and initializes the <b>Package</b> for this
-   * model, and for any others upon which it depends.  Simple
-   * dependencies are satisfied by calling this method on all
-   * dependent packages before doing anything else.  This method drives
-   * initialization for interdependent packages directly, in parallel
-   * with this package, itself.
-   * <p>Of this package and its interdependencies, all packages which
-   * have not yet been registered by their URI values are first created
-   * and registered.  The packages are then initialized in two steps:
-   * meta-model objects for all of the packages are created before any
-   * are initialized, since one package's meta-model objects may refer to
-   * those of another.
-   * <p>Invocation of this method will not affect any packages that have
-   * already been initialized.
+   * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+   * 
+   * <p>This method is used to initialize {@link ElementsPackage#eINSTANCE} when that field is accessed.
+   * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
    * @see #eNS_URI
@@ -137,6 +127,9 @@ public class ElementsPackageImpl extends EPackageImpl implements ElementsPackage
     // Mark meta-data to indicate it can't be changed
     theElementsPackage.freeze();
 
+  
+    // Update the registry and return the package
+    EPackage.Registry.INSTANCE.put(ElementsPackage.eNS_URI, theElementsPackage);
     return theElementsPackage;
   }
 
