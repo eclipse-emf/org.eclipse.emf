8b4690b5fd78289dbac689e6fe798ea8b04026a7
davidms <davidms>
2009-05-12 15:54:45 +0000

    [275476] Regen to pick up package init() change

--- plugins/org.eclipse.emf.ecore/src/org/eclipse/emf/ecore/xml/type/impl/XMLTypePackageImpl.java
+++ plugins/org.eclipse.emf.ecore/src/org/eclipse/emf/ecore/xml/type/impl/XMLTypePackageImpl.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: XMLTypePackageImpl.java,v 1.22 2009/04/18 11:38:28 emerks Exp $
+ * $Id: XMLTypePackageImpl.java,v 1.23 2009/05/12 15:54:45 davidms Exp $
  */
 package org.eclipse.emf.ecore.xml.type.impl;
 
@@ -512,20 +512,10 @@ public class XMLTypePackageImpl extends EPackageImpl implements XMLTypePackage
   private static boolean isInited = false;
 
   /**
-   * Creates, registers, and initializes the <b>Package</b> for this
-   * model, and for any others upon which it depends.  Simple
-   * dependencies are satisfied by calling this method on all
-   * dependent packages before doing anything else.  This method drives
-   * initialization for interdependent packages directly, in parallel
-   * with this package, itself.
-   * <p>Of this package and its interdependencies, all packages which
-   * have not yet been registered by their URI values are first created
-   * and registered.  The packages are then initialized in two steps:
-   * meta-model objects for all of the packages are created before any
-   * are initialized, since one package's meta-model objects may refer to
-   * those of another.
-   * <p>Invocation of this method will not affect any packages that have
-   * already been initialized.
+   * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+   * 
+   * <p>This method is used to initialize {@link XMLTypePackage#eINSTANCE} when that field is accessed.
+   * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
    * @see #eNS_URI
@@ -562,6 +552,9 @@ public class XMLTypePackageImpl extends EPackageImpl implements XMLTypePackage
     // Mark meta-data to indicate it can't be changed
     theXMLTypePackage.freeze();
 
+  
+    // Update the registry and return the package
+    EPackage.Registry.INSTANCE.put(XMLTypePackage.eNS_URI, theXMLTypePackage);
     return theXMLTypePackage;
   }
 
