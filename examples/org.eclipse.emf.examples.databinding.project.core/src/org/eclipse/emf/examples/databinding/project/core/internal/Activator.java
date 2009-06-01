81cfdce0f7d0dee7ab7aa81347a6967f4fb5f160
tschindl <tschindl>
2009-06-01 17:19:28 +0000

    [278393] Fixed copyright header

--- examples/org.eclipse.emf.examples.databinding.project.core/src/org/eclipse/emf/examples/databinding/project/core/internal/Activator.java
+++ examples/org.eclipse.emf.examples.databinding.project.core/src/org/eclipse/emf/examples/databinding/project/core/internal/Activator.java
@@ -1,50 +1,72 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2009 Bestsolution.at and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ *   Tom Schindl<tom.schindl@bestsolution.at> - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: Activator.java,v 1.2 2009/06/01 17:19:28 tschindl Exp $
+ */
 package org.eclipse.emf.examples.databinding.project.core.internal;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
+
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class Activator extends Plugin
+{
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.emf.examples.databinding.project.core";
+  // The plug-in ID
+  public static final String PLUGIN_ID = "org.eclipse.emf.examples.databinding.project.core";
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+  // The shared instance
+  private static Activator plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+  /**
+   * The constructor
+   */
+  public Activator()
+  {
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+   */
+  public void start(BundleContext context) throws Exception
+  {
+    super.start(context);
+    plugin = this;
+  }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext context) throws Exception
+  {
+    plugin = null;
+    super.stop(context);
+  }
+
+  /**
+   * Returns the shared instance
+   *
+   * @return the shared instance
+   */
+  public static Activator getDefault()
+  {
+    return plugin;
+  }
 
 }
