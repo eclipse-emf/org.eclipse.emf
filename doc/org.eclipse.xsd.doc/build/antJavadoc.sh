b26801bcd4db1da3c9513649901ad0e8e24ab718
davidms <davidms>
2009-08-12 23:05:48 +0000

    [286437] Reinstate the change from bug 281779

--- doc/org.eclipse.xsd.doc/build/antJavadoc.sh
+++ doc/org.eclipse.xsd.doc/build/antJavadoc.sh
@@ -72,13 +72,13 @@ groupPackage org.eclipse.emf.mapping.xsd2ecore.editor
 # The directory of the plugins in the order they were built
 excludes="resources.jar";
 includes="org.eclipse.xsd|org.eclipse.emf.mapping.xsd2ecore";
-pluginDirs=`find $eclipseDir/plugins -name @dot -printf '%T@ %p\n' | sort -n | egrep "$includes" | egrep -v "$excludes" | cut -f2 -d' ' | sed -e 's/\(\/.*\)\/.*/\1/'`
+pluginDirs=`find -L $eclipseDir/plugins -name @dot -printf '%T@ %p\n' | sort -n | egrep "$includes" | egrep -v "$excludes" | cut -f2 -d' ' | sed -e 's/\(\/.*\)\/.*/\1/'`
 
 ### TODO: missing emf/xsd plugins (?) in $eclipseDir - need to copy them over or reference source (?)
 ### so that all classes/packages (and thus @links) can be resolved
 
 # All the jars in the plugins directory
-classpath=`find $eclipseDir/plugins \( -name "*.jar" -type f \) -o \( -name '@dot' -type d \) | tr '\n' ':'`; 
+classpath=`find -L $eclipseDir/plugins \( -name "*.jar" -type f \) -o \( -name '@dot' -type d \) | tr '\n' ':'`; 
 if [ $debug -gt 1 ]; then echo "Got classpath: "; echo $classpath | perl -pe "s#:#\n\t:#g"; fi
 
 # Calculates the packagesets and the calls to copyDocFiles (used in ${antScript}.template)
