424588bed1c0a503720e0f6481489e2c62cc9889
nickb <nickb>
2010-03-16 17:20:22 +0000

    typo

--- releng/org.eclipse.emf.releng.athena/util/unpackUpdateSiteJars.sh
+++ releng/org.eclipse.emf.releng.athena/util/unpackUpdateSiteJars.sh
@@ -5,7 +5,7 @@ if [[ $1 ]]; then
 	updateDir="$1";
 else
 	echo "Must specify dir to search and unpack, eg."
-	echo"  $0 /home/data/httpd/download.eclipse.org/modeling/emf/emf/updates/2.6"
+	echo " $0 /home/data/httpd/download.eclipse.org/modeling/emf/emf/updates/2.6"
 	exit 1;
 fi
 
