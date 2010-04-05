950a98713e858a2091b85494223e371f00de3575
emerks <emerks>
2010-04-05 15:30:53 +0000

    [283499] Support multi-list for single valued properties.

--- plugins/org.eclipse.emf.databinding/src/org/eclipse/emf/databinding/internal/EMFListProperty.java
+++ plugins/org.eclipse.emf.databinding/src/org/eclipse/emf/databinding/internal/EMFListProperty.java
@@ -13,11 +13,13 @@
  *   Tom Schindl <tom.schindl@bestsolution.at> - port to EMF in 262160
  * </copyright>
  *
- * $Id: EMFListProperty.java,v 1.3 2010/02/04 20:56:28 emerks Exp $
+ * $Id: EMFListProperty.java,v 1.4 2010/04/05 15:30:53 emerks Exp $
  */
 package org.eclipse.emf.databinding.internal;
 
+import java.util.AbstractSequentialList;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.eclipse.core.databinding.observable.list.ListDiff;
 import org.eclipse.core.databinding.property.INativePropertyListener;
@@ -26,6 +28,7 @@ import org.eclipse.core.databinding.property.ISimplePropertyListener;
 import org.eclipse.core.databinding.property.list.SimpleListProperty;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.FeatureMapUtil;
 
 
 /**
@@ -61,8 +64,120 @@ public class EMFListProperty extends SimpleListProperty
   @Override
   protected List< ? > doGetList(Object source)
   {
-    EObject eObj = (EObject)source;
-    return (List< ? >)eObj.eGet(eStructuralFeature);
+    final EObject eObject = (EObject)source;
+    if (FeatureMapUtil.isMany(eObject, eStructuralFeature))
+    {
+      return (List< ? >)eObject.eGet(eStructuralFeature);
+    }
+    else
+    {
+      return 
+        new AbstractSequentialList<Object>()
+        {
+          @Override
+          public ListIterator<Object> listIterator(int index)
+          {
+            ListIterator<Object> result =
+              new ListIterator<Object>()
+              {
+                protected int position = 0;
+                protected boolean setOrRemoveAllowed;
+                public void add(Object o)
+                {
+                  if (position != 0)
+                  {
+                    throw new IllegalStateException();
+                  }
+                  eObject.eSet(eStructuralFeature, o);
+                  position = 1;
+                  setOrRemoveAllowed = false;
+                }
+
+                public boolean hasNext()
+                {
+                  return position == 0 && size() == 1;
+                }
+
+                public boolean hasPrevious()
+                {
+                  return position == 1;
+                }
+
+                public Object next()
+                {
+                  if (!hasNext())
+                  {
+                    throw new IllegalStateException();
+                  }
+                  ++position;
+                  setOrRemoveAllowed = true;
+                  return eObject.eGet(eStructuralFeature);
+                }
+
+                public int nextIndex()
+                {
+                  return position;
+                }
+
+                public Object previous()
+                {
+                  if (!hasPrevious())
+                  {
+                    throw new IllegalStateException();
+                  }
+                  else
+                  {
+                    --position;
+                    setOrRemoveAllowed = true;
+                    return eObject.eGet(eStructuralFeature);
+                  }
+                }
+
+                public int previousIndex()
+                {
+                  return position - 1;
+                }
+
+                public void remove()
+                {
+                  if (!setOrRemoveAllowed)
+                  {
+                    throw new IllegalStateException();
+                  }
+                  else
+                  {
+                    setOrRemoveAllowed = false;
+                    eObject.eUnset(eStructuralFeature);
+                  }
+                }
+
+                public void set(Object o)
+                {
+                  if (!setOrRemoveAllowed)
+                  {
+                    throw new IllegalStateException();
+                  }
+                  else
+                  {
+                    setOrRemoveAllowed = false;
+                    eObject.eSet(eStructuralFeature, o);
+                  }
+                }
+              };
+            for (int i = 0; i < index; ++i)
+            {
+              result.next();
+            }
+            return result;
+          }
+
+          @Override
+          public int size()
+          {
+            return eStructuralFeature.isUnsettable() ? eObject.eIsSet(eStructuralFeature) ? 1 : 0 : eObject.eGet(eStructuralFeature, false) == null ? 0 : 1;
+          }
+        };
+    }
   }
 
   @Override
