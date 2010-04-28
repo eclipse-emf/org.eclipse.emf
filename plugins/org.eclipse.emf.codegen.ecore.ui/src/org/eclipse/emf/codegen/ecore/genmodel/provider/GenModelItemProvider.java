198f57e4b75840deeec5f1d64b04c634acc15d1e
emerks <emerks>
2010-04-28 14:50:57 +0000

    [310835] Support generating against the GWT runtime platform.

--- plugins/org.eclipse.emf.codegen.ecore.ui/src/org/eclipse/emf/codegen/ecore/genmodel/provider/GenModelItemProvider.java
+++ plugins/org.eclipse.emf.codegen.ecore.ui/src/org/eclipse/emf/codegen/ecore/genmodel/provider/GenModelItemProvider.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: GenModelItemProvider.java,v 1.46 2010/03/11 02:31:18 khussey Exp $
+ * $Id: GenModelItemProvider.java,v 1.47 2010/04/28 14:50:57 emerks Exp $
  */
 package org.eclipse.emf.codegen.ecore.genmodel.provider;
 
@@ -145,6 +145,7 @@ public class GenModelItemProvider
       addClassNamePatternPropertyDescriptor(object);
       addOperationReflectionPropertyDescriptor(object);
       addRichAjaxPlatformPropertyDescriptor(object);
+      addRuntimePlatformPropertyDescriptor(object);
     }
     return itemPropertyDescriptors;
   }
@@ -1806,6 +1807,29 @@ public class GenModelItemProvider
   }
 
   /**
+   * This adds a property descriptor for the Runtime Platform feature.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected void addRuntimePlatformPropertyDescriptor(Object object)
+  {
+    itemPropertyDescriptors.add
+      (createItemPropertyDescriptor
+        (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+         getResourceLocator(),
+         getString("_UI_GenModel_runtimePlatform_feature"),
+         getString("_UI_GenModel_runtimePlatform_description"),
+         GenModelPackage.Literals.GEN_MODEL__RUNTIME_PLATFORM,
+         true,
+         false,
+         false,
+         ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+         getString("_UI_AllPropertyCategory"),
+         null));
+  }
+
+  /**
    * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
    * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
    * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
@@ -1947,6 +1971,7 @@ public class GenModelItemProvider
       case GenModelPackage.GEN_MODEL__CLASS_NAME_PATTERN:
       case GenModelPackage.GEN_MODEL__OPERATION_REFLECTION:
       case GenModelPackage.GEN_MODEL__RICH_AJAX_PLATFORM:
+      case GenModelPackage.GEN_MODEL__RUNTIME_PLATFORM:
         fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
         return;
       case GenModelPackage.GEN_MODEL__GEN_PACKAGES:
