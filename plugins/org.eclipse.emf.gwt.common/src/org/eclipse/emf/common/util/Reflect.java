bb8f34431bc8e11ee40dd537963c973f1592810d
emerks <emerks>
2011-01-17 19:50:26 +0000

    [334296] Support single sourcing of models that also target the GWT runtime.

--- plugins/org.eclipse.emf.gwt.common/src/org/eclipse/emf/common/util/Reflect.java
+++ plugins/org.eclipse.emf.gwt.common/src/org/eclipse/emf/common/util/Reflect.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2010 Ed Merks and others.
+ * Copyright (c) 2010-2011 Ed Merks and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,28 +12,52 @@
  *
  * </copyright>
  *
- * $Id: Reflect.java,v 1.1 2010/04/28 14:49:26 emerks Exp $
+ * $Id: Reflect.java,v 1.2 2011/01/17 19:50:26 emerks Exp $
  */
 package org.eclipse.emf.common.util;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A utility class for helping implement reflective capabilities not available with Google Widget Toolkit,
+ * i.e., specifically the ability to {@link Class#isInstance(Object) instance test} and to {@link Array#newInstance(Class, int) create typed arrays}.
+ * @since 2.7
+ */
 public final class Reflect
 {
+  /**
+   * A global registry of helper instances.
+   */
   static final Map<Class<?>, Helper> HELPER_REGISTRY = new HashMap<Class<?>, Reflect.Helper>();
 
+  /**
+   * An interface implemented by reflective helpers.
+   */
   public interface Helper
   {
+    /**
+     * Do an {@link Class#isInstance(Object) instanceof test}.
+     */
     boolean isInstance(Object instance);
+
+    /**
+     * Create a {@link Array#newInstance(Class, int) typed array}.
+     */
     Object newArrayInstance(int size);
   }
   
+  /**
+   * Register a helper for the given class.
+   */
   public static void register(Class<?> class_, Helper helper)
   {
     HELPER_REGISTRY.put(class_, helper);
   }
 
+  /**
+   * Do an {@link Class#isInstance(Object) instanceof test}.
+   */
   public static boolean isInstance(Class<?> class_, Object instance)
   {
     Helper helper = HELPER_REGISTRY.get(class_);
