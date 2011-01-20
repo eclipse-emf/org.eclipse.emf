2184349e83ac1b8a669f784def1b00f7688d9763
emerks <emerks>
2011-01-20 01:09:41 +0000

    [287249] Support switch composition and regenerate all the models to use it.

--- plugins/org.eclipse.emf.mapping.ecore2ecore/src/org/eclipse/emf/mapping/ecore2ecore/util/Ecore2EcoreSwitch.java
+++ plugins/org.eclipse.emf.mapping.ecore2ecore/src/org/eclipse/emf/mapping/ecore2ecore/util/Ecore2EcoreSwitch.java
@@ -12,16 +12,15 @@
  *
  * </copyright>
  *
- * $Id: Ecore2EcoreSwitch.java,v 1.8 2007/06/12 21:15:32 emerks Exp $
+ * $Id: Ecore2EcoreSwitch.java,v 1.9 2011/01/20 01:09:41 emerks Exp $
  */
 package org.eclipse.emf.mapping.ecore2ecore.util;
 
 
-import java.util.List;
-
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.util.Switch;
 import org.eclipse.emf.mapping.Mapping;
 import org.eclipse.emf.mapping.MappingRoot;
 
@@ -41,7 +40,7 @@ import org.eclipse.emf.mapping.ecore2ecore.*;
  * @see org.eclipse.emf.mapping.ecore2ecore.Ecore2EcorePackage
  * @generated
  */
-public class Ecore2EcoreSwitch<T> {
+public class Ecore2EcoreSwitch<T> extends Switch<T> {
   /**
    * The cached model package
    * <!-- begin-user-doc -->
@@ -65,15 +64,17 @@ public class Ecore2EcoreSwitch<T> {
   }
 
   /**
-   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * Checks whether this is a switch for the given package.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @parameter ePackage the package in question.
+   * @return whether this is a switch for the given package.
    * @generated
    */
-  public T doSwitch(EObject theEObject)
+  @Override
+  protected boolean isSwitchFor(EPackage ePackage)
   {
-    return doSwitch(theEObject.eClass(), theEObject);
+    return ePackage == modelPackage;
   }
 
   /**
@@ -83,29 +84,7 @@ public class Ecore2EcoreSwitch<T> {
    * @return the first non-null result returned by a <code>caseXXX</code> call.
    * @generated
    */
-  protected T doSwitch(EClass theEClass, EObject theEObject)
-  {
-    if (theEClass.eContainer() == modelPackage)
-    {
-      return doSwitch(theEClass.getClassifierID(), theEObject);
-    }
-    else
-    {
-      List<EClass> eSuperTypes = theEClass.getESuperTypes();
-      return
-        eSuperTypes.isEmpty() ?
-          defaultCase(theEObject) :
-          doSwitch(eSuperTypes.get(0), theEObject);
-    }
-  }
-
-  /**
-   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @return the first non-null result returned by a <code>caseXXX</code> call.
-   * @generated
-   */
+  @Override
   protected T doSwitch(int classifierID, EObject theEObject)
   {
     switch (classifierID)
@@ -182,6 +161,7 @@ public class Ecore2EcoreSwitch<T> {
    * @see #doSwitch(org.eclipse.emf.ecore.EObject)
    * @generated
    */
+  @Override
   public T defaultCase(EObject object)
   {
     return null;
