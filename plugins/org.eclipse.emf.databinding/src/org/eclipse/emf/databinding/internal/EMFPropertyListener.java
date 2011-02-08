e285b5b8360ad336fb5385a928dae75e5c74e872
tschindl <tschindl>
2011-02-08 22:28:34 +0000

    Bug 331553 - EMFPropertyListener.EMFMapPropertyListener doesn't handle all the different types of notifications that can occur

--- plugins/org.eclipse.emf.databinding/src/org/eclipse/emf/databinding/internal/EMFPropertyListener.java
+++ plugins/org.eclipse.emf.databinding/src/org/eclipse/emf/databinding/internal/EMFPropertyListener.java
@@ -11,25 +11,30 @@
  *   Tom Schindl <tom.schindl@bestsolution.at> - port to EMF in 262160
  * </copyright>
  *
- * $Id: EMFPropertyListener.java,v 1.6 2010/05/04 12:59:27 tschindl Exp $
+ * $Id: EMFPropertyListener.java,v 1.7 2011/02/08 22:28:34 tschindl Exp $
  */
 package org.eclipse.emf.databinding.internal;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
 import org.eclipse.core.databinding.observable.Diffs;
 import org.eclipse.core.databinding.observable.list.ListDiff;
 import org.eclipse.core.databinding.observable.list.ListDiffEntry;
+import org.eclipse.core.databinding.observable.map.MapDiff;
 import org.eclipse.core.databinding.observable.set.SetDiff;
 import org.eclipse.core.databinding.property.INativePropertyListener;
 import org.eclipse.core.databinding.property.IProperty;
 import org.eclipse.core.databinding.property.ISimplePropertyListener;
 import org.eclipse.core.databinding.property.SimplePropertyEvent;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
@@ -224,15 +229,114 @@ public abstract class EMFPropertyListener extends AdapterImpl implements INative
    */
   public abstract static class EMFMapPropertyListener extends EMFPropertyListener
   {
+		@Override
+		public void addTo(Object source)
+		{
+			super.addTo(source);
+
+			if (source != null)
+			{
+				EMap<?, ?> map = (EMap<?, ?>) ((EObject) source).eGet(getFeature());
+
+				for (Map.Entry<?, ?> entry : map)
+				{
+					((Notifier) entry).eAdapters().add(this);
+				}
+			}
+		}
+
+		@Override
+		public void removeFrom(Object source)
+		{
+			if (source != null)
+			{
+				EMap<?, ?> map = (EMap<?, ?>) ((EObject) source).eGet(getFeature());
+
+				for (Map.Entry<?, ?> entry : map)
+				{
+					((Notifier) entry).eAdapters().remove(this);
+				}
+			}
+
+			super.removeFrom(source);
+		}
+	  
     @Override
     public void notifyChanged(Notification msg)
     {
-      if (getFeature() == msg.getFeature() && !msg.isTouch())
+      if (msg.isTouch())
+    	return;
+      
+      if (getFeature() == msg.getFeature())
       {
-        getListener().handleEvent(
-          new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), Diffs.computeMapDiff(
-            (Map< ? , ? >)msg.getOldValue(),
-            (Map< ? , ? >)msg.getNewValue())));
+        final MapDiff diff;
+        switch (msg.getEventType())
+        {
+          case Notification.ADD: {
+            Map.Entry<?, ?> entry = (Map.Entry<?, ?>)msg.getNewValue();
+            ((EObject) entry).eAdapters().add(this);
+            diff = Diffs.createMapDiffSingleAdd(entry.getKey(), entry.getValue());
+            break;
+          }
+          case Notification.ADD_MANY: {
+            @SuppressWarnings("unchecked")
+            Collection<Map.Entry<?, ?>> newEntries = (Collection<Map.Entry<?, ?>>)msg.getNewValue();
+            Map<Object, Object> newValues = new HashMap<Object, Object>();
+            for (Map.Entry<?, ?> entry : newEntries)
+            {
+              ((EObject) entry).eAdapters().add(this);
+              newValues.put(entry.getKey(), entry.getValue());
+            }
+            diff = Diffs.createMapDiff(newValues.keySet(), Collections.emptySet(), Collections.emptySet(), Collections.emptyMap(), newValues);
+            break;
+          }
+          case Notification.REMOVE: {
+            Map.Entry<?, ?> entry = (Map.Entry<?, ?>)msg.getOldValue();
+            ((EObject) entry).eAdapters().remove(this);
+            diff = Diffs.createMapDiffSingleRemove(entry.getKey(), entry.getValue());
+            break;
+          }
+          case Notification.REMOVE_MANY: {
+            @SuppressWarnings("unchecked")
+            Collection<Map.Entry<?, ?>> oldEntries = (Collection<Map.Entry<?, ?>>)msg.getOldValue();
+            Map<Object, Object> oldValues = new HashMap<Object, Object>();
+            for (Map.Entry<?, ?> entry : oldEntries)
+            {
+              ((EObject) entry).eAdapters().remove(this);
+              oldValues.put(entry.getKey(), entry.getValue());
+            }
+            diff = Diffs.createMapDiff(Collections.emptySet(), oldValues.keySet(), Collections.emptySet(), oldValues, Collections.emptyMap());
+            break;
+          }
+          case Notification.SET:
+          case Notification.RESOLVE: {
+            Map.Entry<?, ?> oldEntry = (Map.Entry<?, ?>)msg.getOldValue();
+            ((EObject) oldEntry).eAdapters().remove(this);
+            Map<Object, Object> oldValues = new HashMap<Object, Object>();
+            oldValues.put(oldEntry.getKey(), oldEntry.getValue());
+            Map.Entry<?, ?> newEntry = (Map.Entry<?, ?>)msg.getNewValue();
+            ((EObject) newEntry).eAdapters().add(this);
+            Map<Object, Object> newValues = new HashMap<Object, Object>();
+            newValues.put(newEntry.getKey(), newEntry.getValue());
+            diff = Diffs.createMapDiff(newValues.keySet(), oldValues.keySet(), Collections.emptySet(), oldValues, newValues);
+            break;
+          }
+          case Notification.MOVE:
+          case Notification.UNSET: {
+            // This just represents going back to the unset state, but
+            // that doesn't affect the contents of the list.
+            //
+            return;
+          }
+          default: {
+            throw new RuntimeException("unhandled case");
+          }
+        }
+        getListener().handleEvent((new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), diff)));
+      } else if (((EClass) getFeature().getEType()).getEStructuralFeature("value") == msg.getFeature()) {
+      	Object key = ((Map.Entry<?, ?>) msg.getNotifier()).getKey();
+		MapDiff diff = Diffs.createMapDiffSingleChange(key, msg.getOldValue(), msg.getNewValue());
+      	getListener().handleEvent(new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), diff));
       }
     }
   }
