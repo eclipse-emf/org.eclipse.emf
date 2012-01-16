bb7e880f798da96e59f1d3d8ce8ecb1de34f123d
Ed Merks <ed.merks@gmail.com>
2012-01-16 19:09:12 +0100

    [368320] Provide nice icons and text

--- plugins/org.eclipse.emf.ecore.xcore.ui/src/org/eclipse/emf/ecore/xcore/ui/EmptyXcoreProjectWizard.java
+++ plugins/org.eclipse.emf.ecore.xcore.ui/src/org/eclipse/emf/ecore/xcore/ui/EmptyXcoreProjectWizard.java
@@ -15,16 +15,42 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.codegen.ecore.ui.EmptyProjectWizard;
 import org.eclipse.emf.codegen.util.CodeGenUtil;
+import org.eclipse.emf.ecore.xcore.ui.internal.XcoreActivator;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.xtext.ui.XtextProjectHelper;
 
 
 public class EmptyXcoreProjectWizard extends EmptyProjectWizard
 {
+  @Override
+  public void init(IWorkbench workbench, IStructuredSelection selection)
+  {
+    this.workbench = workbench;
+    this.selection = selection;
+    setDefaultPageImageDescriptor(ExtendedImageRegistry.INSTANCE.getImageDescriptor(XcoreActivator.getInstance().getBundle().getEntry("icons/full/wizban/NewXcoreProject.gif")));
+    setWindowTitle(Platform.getResourceBundle(XcoreActivator.getInstance().getBundle()).getString("_UI_NewXcoreProject_title"));
+  }
+
+  @Override
+  public void addPages()
+  {
+    // TODO Auto-generated method stub
+    super.addPages();
+    IWizardPage[] pages = getPages();
+    IWizardPage page = pages[pages.length - 1];
+    page.setTitle(Platform.getResourceBundle(XcoreActivator.getInstance().getBundle()).getString("_UI_NewXcoreProject_title"));
+    page.setDescription(Platform.getResourceBundle(XcoreActivator.getInstance().getBundle()).getString("_UI_NewXcoreProject_description"));
+  }
+
   @Override
   public void modifyWorkspace(IProgressMonitor progressMonitor) throws CoreException, UnsupportedEncodingException, IOException
   {
