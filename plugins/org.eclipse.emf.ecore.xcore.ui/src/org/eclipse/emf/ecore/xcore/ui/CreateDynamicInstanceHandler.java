46bf43d7f71f79ec27d265639d0f4c81806b2638
Ed Merks <ed.merks@gmail.com>
2013-07-11 16:41:30 +0200

    [410483] Provide key bindings for organize imports and dynamic instance
    Change-Id: I56f3d4929a68acf99676ae75a8a89d5a34327ef0

--- plugins/org.eclipse.emf.ecore.xcore.ui/src/org/eclipse/emf/ecore/xcore/ui/CreateDynamicInstanceHandler.java
+++ plugins/org.eclipse.emf.ecore.xcore.ui/src/org/eclipse/emf/ecore/xcore/ui/CreateDynamicInstanceHandler.java
@@ -11,6 +11,7 @@ package org.eclipse.emf.ecore.xcore.ui;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
@@ -23,6 +24,7 @@ import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.ISources;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
 import org.eclipse.xtext.resource.XtextResource;
@@ -47,43 +49,77 @@ public class CreateDynamicInstanceHandler extends AbstractHandler
   {
     XtextEditor xtextEditor = EditorUtils.getActiveXtextEditor(event);
     final ITextSelection selection = (ITextSelection)xtextEditor.getSelectionProvider().getSelection();
-    // EObject element = eObjectAtOffsetHelper.resolveElementAt(localResource, selection.getOffset());
-    xtextEditor.getDocument().readOnly(new IUnitOfWork<Object, XtextResource>()
-      {
-        public Object exec(XtextResource localResource) throws Exception
-        {
-          Object element = eObjectAtOffsetHelper.resolveElementAt(localResource, selection.getOffset());
-          if (element instanceof XClass)
-          {
-            EClass eClass = (EClass)mapper.getEcore((XClass)element);
-            if (eClass != null)
-            {
-              URI uri = eClass.eResource().getURI();
-              IStructuredSelection selection = StructuredSelection.EMPTY;
-              if (uri != null && uri.isHierarchical())
-              {
-                if (uri.isRelative() || (uri = uri.deresolve(PLATFORM_RESOURCE)).isRelative())
-                {
-                  IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uri.toString()));
-                  if (file.exists())
-                  {
-                    selection = new StructuredSelection(file);
-                  }
-                }
-              }
-
-              DynamicModelWizard dynamicModelWizard = new DynamicModelWizard(eClass);
-              dynamicModelWizard.init(PlatformUI.getWorkbench(), selection);
-              WizardDialog wizardDialog = new WizardDialog(
-                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-                dynamicModelWizard);
-
-              wizardDialog.open();
-            }
-          }
-          return null;
-        }
-      });
+    xtextEditor.getDocument().readOnly
+      (new IUnitOfWork<Object, XtextResource>()
+       {
+         public Object exec(XtextResource localResource) throws Exception
+         {
+           Object element = eObjectAtOffsetHelper.resolveElementAt(localResource, selection.getOffset());
+           if (element instanceof XClass)
+           {
+             EClass eClass = (EClass)mapper.getEcore((XClass)element);
+             if (eClass != null)
+             {
+               URI uri = eClass.eResource().getURI();
+               IStructuredSelection selection = StructuredSelection.EMPTY;
+               if (uri != null && uri.isHierarchical())
+               {
+                 if (uri.isRelative() || (uri = uri.deresolve(PLATFORM_RESOURCE)).isRelative())
+                 {
+                   IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uri.toString()));
+                   if (file.exists())
+                   {
+                     selection = new StructuredSelection(file);
+                   }
+                 }
+               }
+ 
+               DynamicModelWizard dynamicModelWizard = new DynamicModelWizard(eClass);
+               dynamicModelWizard.init(PlatformUI.getWorkbench(), selection);
+               WizardDialog wizardDialog = new WizardDialog(
+                 PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+                 dynamicModelWizard);
+ 
+               wizardDialog.open();
+             }
+           }
+           return null;
+         }
+       });
     return null;
+  }
+  
+  @Override
+  public void setEnabled(Object evaluationContext)
+  {
+    boolean isEnabled = false;
+    if (evaluationContext instanceof IEvaluationContext) 
+    {
+      Object activeEditor = ((IEvaluationContext) evaluationContext).getVariable(ISources.ACTIVE_EDITOR_NAME);
+      if (activeEditor instanceof XtextEditor) 
+      {
+        XtextEditor xtextEditor = (XtextEditor) activeEditor;
+        final ITextSelection selection = (ITextSelection)xtextEditor.getSelectionProvider().getSelection();
+        isEnabled = 
+          xtextEditor.getDocument().readOnly
+            (new IUnitOfWork<Boolean, XtextResource>()
+             {
+               public Boolean exec(XtextResource localResource) throws Exception
+               {
+                 Object element = eObjectAtOffsetHelper.resolveElementAt(localResource, selection.getOffset());
+                 if (element instanceof XClass)
+                 {
+                   EClass eClass = (EClass)mapper.getEcore((XClass)element);
+                   if (eClass != null)
+                   {
+                     return true;
+                   }
+                 }
+                 return false;
+               }
+             });
+          }
+    }
+    setBaseEnabled(isEnabled);
   }
 }
