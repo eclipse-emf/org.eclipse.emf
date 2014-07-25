3427874c6fb010c3ff4dea732fd3473f49c83d9f
Stefan Oehme <stefan.oehme@itemis.de>
2014-07-25 09:09:18 +0200

    only unload elements that are actually referable
    Change-Id: I17a807a0ebccc1d36283f81dc6684c9e768d397f
    Signed-off-by: Stefan Oehme <stefan.oehme@itemis.de>

--- plugins/org.eclipse.emf.ecore.xcore/src/org/eclipse/emf/ecore/xcore/resource/XcoreReferableElementsUnloader.java
+++ plugins/org.eclipse.emf.ecore.xcore/src/org/eclipse/emf/ecore/xcore/resource/XcoreReferableElementsUnloader.java
@@ -11,32 +11,52 @@ package org.eclipse.emf.ecore.xcore.resource;
 import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenAnnotation;
+import org.eclipse.emf.codegen.ecore.genmodel.GenClassifier;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xcore.XAnnotation;
+import org.eclipse.emf.ecore.xcore.XClassifier;
+import org.eclipse.xtext.common.types.JvmDeclaredType;
+import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.parser.antlr.IReferableElementsUnloader;
 import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.util.Tuples;
-
 
 public class XcoreReferableElementsUnloader implements IReferableElementsUnloader
 {
   public void unloadRoot(EObject root)
   {
     List<Pair<EObject, URI>> elementsToUnload = newArrayList();
-    for (Iterator<EObject> i = EcoreUtil.getAllProperContents(Collections.singleton(root), false); i.hasNext();)
+    Resource resource = root.eResource();
+    URI uri = resource.getURI();
+    for (TreeIterator<EObject> i = EcoreUtil.getAllProperContents(Collections.singleton(root), false); i.hasNext();)
     {
       EObject element = i.next();
-      elementsToUnload.add(Tuples.create(element, EcoreUtil.getURI(element)));
+      if (element instanceof GenClassifier
+          || element instanceof GenAnnotation
+          || element instanceof XClassifier
+          || element instanceof XAnnotation
+          || element instanceof JvmDeclaredType
+          || element instanceof JvmTypeReference)
+      {
+        i.prune();
+      }
+
+      URI proxyURI = uri.appendFragment(resource.getURIFragment(element));
+      elementsToUnload.add(Tuples.create(element, proxyURI));
       element.eAdapters().clear();
     }
+
     for (Pair<EObject, URI> elementToUnload : elementsToUnload)
     {
-      ((InternalEObject)elementToUnload.getFirst()).eSetProxyURI(elementToUnload.getSecond());
+      ((InternalEObject) elementToUnload.getFirst()).eSetProxyURI(elementToUnload.getSecond());
     }
   }
 }
