bc8d593cc7e57eb1a9dd87dce03d1daad8b387ba
Eike Stepper <stepper@esc-net.de>
2018-11-05 09:43:17 +0100

    [540774] Wrong value in EObjectValidator.EOBJECT_DIAGNOSTIC_CODE_COUNT
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=540774
    Change-Id: Ia539f83127b0a654902ca58136069d8b007a1cd4
    Signed-off-by: Eike Stepper <stepper@esc-net.de>

--- plugins/org.eclipse.emf.ecore/src/org/eclipse/emf/ecore/util/EObjectValidator.java
+++ plugins/org.eclipse.emf.ecore/src/org/eclipse/emf/ecore/util/EObjectValidator.java
@@ -77,13 +77,13 @@ public class EObjectValidator implements EValidator
   public static final int EOBJECT__NO_CIRCULAR_CONTAINMENT = 15;
   public static final int EOBJECT__EVERY_BIDIRECTIONAL_REFERENCE_IS_PAIRED = 16;
   
-  static final int EOBJECT_DIAGNOSTIC_CODE_COUNT = EOBJECT__NO_CIRCULAR_CONTAINMENT;
+  static final int EOBJECT_DIAGNOSTIC_CODE_COUNT = EOBJECT__EVERY_BIDIRECTIONAL_REFERENCE_IS_PAIRED;
   
   /**
    * A key to be used in <code>context</code> maps to indicate the root object at which validation started.
    * It's used to detect {@link #EOBJECT__NO_CIRCULAR_CONTAINMENT circular containment}
    * and to prevent {@link Diagnostician#validate(EClass, EObject, DiagnosticChain, Map) infinite recursion}.
-   * The value of the entry must be the root{@link EObject}.
+   * The value of the entry must be the root {@link EObject}.
    * @see EValidator#validate(EObject, DiagnosticChain, Map)
    * @see #validate_NoCircularContainment(EObject, DiagnosticChain, Map)
    * @since 2.5
