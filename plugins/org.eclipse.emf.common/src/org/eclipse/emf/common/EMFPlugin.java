d7db97da1d11d9c7126265ec7a038bcc0bab3ebd
Ed Willink <ed@willink.me.uk>
2025-09-10 17:41:26 +0100

    [#80] Avoid unnecessary standalone NPE

--- plugins/org.eclipse.emf.common/src/org/eclipse/emf/common/EMFPlugin.java
+++ plugins/org.eclipse.emf.common/src/org/eclipse/emf/common/EMFPlugin.java
@@ -82,10 +82,13 @@ public abstract class EMFPlugin extends DelegatingResourceLocator implements Res
     boolean result = false;
     try
     {
-      // With this we make sure, that we are really in a proper Eclipse Environment with everything we need.
-      // Some supplements pretend that the Platform is running, but don't provide a ExtensionRegistry. 
-      FrameworkUtil.getBundle(EMFPlugin.class).loadClass("org.eclipse.core.runtime.IRegistryChangeListener");
-      result = Platform.isRunning();
+      if (Platform.isRunning())
+      {
+        // With this we make sure, that we are really in a proper Eclipse Environment with everything we need.
+        // Some supplements pretend that the Platform is running, but don't provide a ExtensionRegistry.
+        FrameworkUtil.getBundle(EMFPlugin.class).loadClass("org.eclipse.core.runtime.IRegistryChangeListener");
+        result = true;
+      }
     }
     catch (Throwable exception)
     {
